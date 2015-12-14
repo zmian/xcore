e867b01864fe8890899ba1b50d0365fa03816388
Zeeshan Mian <zmian@me.com>
2015-12-13 22:22:46 -0500

    Added third-party code: NSTimer+Closure and Dispatch

---
+++ Source/ThirdParty/Dispatch.swift
@@ -0,0 +1,47 @@
+//
+//  Dispatch.swift
+//  Inferis
+//
+
+import Foundation
+
+public final class dispatch {
+    public class async {
+        public class func bg(block: dispatch_block_t) {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), block)
+        }
+
+        public class func main(block: dispatch_block_t) {
+            dispatch_async(dispatch_get_main_queue(), block)
+        }
+    }
+
+    public class sync {
+        public class func bg(block: dispatch_block_t) {
+            dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), block)
+        }
+
+        public class func main(block: dispatch_block_t) {
+            if NSThread.isMainThread()  {
+                block()
+            }
+            else {
+                dispatch_sync(dispatch_get_main_queue(), block)
+            }
+        }
+    }
+
+    public class after {
+        public class func bg(when: dispatch_time_t, block: dispatch_block_t) {
+            dispatch_after(when, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)!, block)
+        }
+
+        public class func main(when: dispatch_time_t, block: dispatch_block_t) {
+            dispatch_after(when, dispatch_get_main_queue(), block)
+        }
+    }
+
+    public class func seconds(interval: NSTimeInterval) -> dispatch_time_t {
+        return dispatch_time(DISPATCH_TIME_NOW, Int64(interval * Double(NSEC_PER_SEC)))
+    }
+}
--- Xcore.podspec
+++ Xcore.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = 'Xcore'
-  s.version      = '1.0.1'
+  s.version      = '1.0.2'
   s.license      = { :type => 'MIT' }
   s.summary      = 'Cocoa Touch Classes + Extensions'
   s.homepage     = 'https://github.com/zmian/xcore.swift'
