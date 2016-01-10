5631a860a55edb5653da41ad359f28bcac759db7
Zeeshan Mian <zmian@me.com>
2016-01-09 22:08:39 -0500

    Added base classes

---
+++ Sources/Classes/BaseTableViewCell.swift
@@ -0,0 +1,76 @@
+//
+// BaseTableViewCell.swift
+//
+// Copyright © 2015 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+public class BaseTableViewCell: UITableViewCell {
+
+    // MARK: Public Interface
+
+    public class var reuseIdentifier: String { return "BaseTableViewCellIdentifier" }
+
+    public var separatorInsets = UIEdgeInsetsZero {
+        didSet { setupSeparatorInset() }
+    }
+
+    // MARK: Init Methods
+
+    public convenience init() {
+        self.init(style: .Default, reuseIdentifier: self.dynamicType.reuseIdentifier)
+    }
+
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    // MARK: Setup Methods
+
+    public override func setSelected(selected: Bool, animated: Bool) {}
+    public override func setHighlighted(highlighted: Bool, animated: Bool) {}
+
+    private func commonInit() {
+        backgroundColor = UIColor.clearColor()
+        setupSeparatorInset()
+        setupSubviews()
+    }
+
+    public func setupSubviews() {}
+
+    private func setupSeparatorInset() {
+        if self.respondsToSelector("setLayoutMargins:") {
+            self.layoutMargins = separatorInsets
+            self.preservesSuperviewLayoutMargins = false
+        }
+
+        if self.respondsToSelector("setSeparatorInset:") {
+            self.separatorInset = separatorInsets
+        }
+    }
+}
