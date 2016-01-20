667bdb8e431871f4356565912194ebf616fa3152
Zeeshan Mian <zmian@me.com>
2016-01-20 00:13:41 -0500

    Comment formatting

--- Sources/ThirdParty/NSTimer+Closure.swift
+++ Sources/ThirdParty/NSTimer+Closure.swift
@@ -6,15 +6,13 @@
 import Foundation
 
 public extension NSTimer {
-    /**
-    Creates and schedules a one-time `NSTimer` instance.
-
-    - Parameters:
-        - delay: The delay before execution.
-        - handler: A closure to execute after `delay`.
-
-    - Returns: The newly-created `NSTimer` instance.
-    */
+    /// Creates and schedules a one-time `NSTimer` instance.
+    ///
+    /// - Parameters:
+    ///    - delay: The delay before execution.
+    ///    - handler: A closure to execute after `delay`.
+    ///
+    /// - Returns: The newly-created `NSTimer` instance.
     public class func schedule(delay delay: NSTimeInterval, handler: NSTimer! -> Void) -> NSTimer {
         let fireDate = delay + CFAbsoluteTimeGetCurrent()
         let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
@@ -22,17 +20,15 @@ public extension NSTimer {
         return timer
     }
 
-    /**
-    Creates and schedules a repeating `NSTimer` instance.
-
-    - Parameters:
-        - repeatInterval: The interval (in seconds) between each execution of
-          `handler`. Note that individual calls may be delayed; subsequent calls
-          to `handler` will be based on the time the timer was created.
-        - handler: A closure to execute at each `repeatInterval`.
-
-    - Returns: The newly-created `NSTimer` instance.
-    */
+    /// Creates and schedules a repeating `NSTimer` instance.
+    ///
+    /// - Parameters:
+    ///     - repeatInterval: The interval (in seconds) between each execution of
+    ///       `handler`. Note that individual calls may be delayed; subsequent calls
+    ///       to `handler` will be based on the time the timer was created.
+    ///     - handler: A closure to execute at each `repeatInterval`.
+    ///
+    /// - Returns: The newly-created `NSTimer` instance.
     public class func schedule(repeatInterval interval: NSTimeInterval, handler: NSTimer! -> Void) -> NSTimer {
         let fireDate = interval + CFAbsoluteTimeGetCurrent()
         let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, handler)
@@ -41,7 +37,7 @@ public extension NSTimer {
     }
 }
 
-public func delayBy(interval: NSTimeInterval, callback: Void -> Void) {
+public func delayBy(interval: NSTimeInterval, callback: () -> Void) {
     NSTimer.schedule(delay: interval) { _ in
         callback()
     }
