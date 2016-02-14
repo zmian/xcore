bf2fe85bf0fc4f6e10e528789f5b456d0d70b9a9
Zeeshan Mian <zmian@me.com>
2016-02-13 20:08:07 -0500

    Added colorize extension to UIImage

--- Sources/Extensions/UIKitExtensions.swift
+++ Sources/Extensions/UIKitExtensions.swift
@@ -848,7 +848,7 @@ public extension UIImage {
         let image = self
         let rect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, image.scale)
-        let c: CGContextRef = UIGraphicsGetCurrentContext()!
+        let c = UIGraphicsGetCurrentContext()!
         image.drawInRect(rect)
         CGContextSetFillColorWithColor(c, color.CGColor)
         CGContextSetBlendMode(c, CGBlendMode.SourceAtop)
@@ -872,6 +872,73 @@ public extension UIImage {
                 completionHandler(resizedImage: tintedImage)
             }
         }
+    }
+
+    // Credit: http://stackoverflow.com/a/34547445
+
+    /// Colorize image with given tint color.
+    ///
+    /// This is similar to Photoshop's **Color** layer blend mode.
+    /// This is perfect for non-greyscale source images, and images that have both highlights and shadows that should be preserved.
+    /// White will stay white and black will stay black as the lightness of the image is preserved.
+    ///
+    /// Sample Result:
+    ///
+    /// <img src="http://yannickstephan.com/easyhelper/tint1.png" height="70" width="120"/>
+    /// <img src="http://yannickstephan.com/easyhelper/tint2.png" height="70" width="120"/>
+    public func colorize(tintColor: UIColor) -> UIImage {
+        return modifiedImage { context, rect in
+            // draw black background - workaround to preserve color of partially transparent pixels
+            CGContextSetBlendMode(context, .Normal)
+            UIColor.blackColor().setFill()
+            CGContextFillRect(context, rect)
+
+            // draw original image
+            CGContextSetBlendMode(context, .Normal)
+            CGContextDrawImage(context, rect, CGImage)
+
+            // tint image (loosing alpha) - the luminosity of the original image is preserved
+            CGContextSetBlendMode(context, .Color)
+            tintColor.setFill()
+            CGContextFillRect(context, rect)
+
+            // mask by alpha values of original image
+            CGContextSetBlendMode(context, .DestinationIn)
+            CGContextDrawImage(context, rect, CGImage)
+        }
+    }
+
+    /// Tint Picto to color
+    ///
+    /// - parameter fillColor: UIColor
+    ///
+    /// - returns: UIImage
+    public func tintPicto(fillColor: UIColor) -> UIImage {
+        return modifiedImage { context, rect in
+            // draw tint color
+            CGContextSetBlendMode(context, .Normal)
+            fillColor.setFill()
+            CGContextFillRect(context, rect)
+
+            // mask by alpha values of original image
+            CGContextSetBlendMode(context, .DestinationIn)
+            CGContextDrawImage(context, rect, CGImage)
+        }
+    }
+
+    /// Modified Image Context, apply modification on image
+    private func modifiedImage(@noescape draw: (CGContext, CGRect) -> ()) -> UIImage {
+        // using scale correctly preserves retina images
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        let context = UIGraphicsGetCurrentContext()!
+        // correctly rotate image
+        CGContextTranslateCTM(context, 0, size.height)
+        CGContextScaleCTM(context, 1, -1)
+        let rect = CGRect(origin: .zero, size: size)
+        draw(context, rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
 }
 
