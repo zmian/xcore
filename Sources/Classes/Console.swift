74d5ef141c54e4f47f6c941ab328c906616dc9db
Zeeshan Mian <zmian@me.com>
2016-02-17 22:33:50 -0500

    Improvements to console struct

--- Sources/Classes/Console.swift
+++ Sources/Classes/Console.swift
@@ -31,39 +31,71 @@ public struct console {
     public static var enableLogLevelWarn  = true
     public static var enableLogLevelError = true
 
-    public static func log(message: String, className: String = __FILE__, lineNumber: Int = __LINE__) {
-        let className = extractClassName(className)
+    /// Writes the textual representations of debug message, separated by separator
+    /// and terminated by terminator, into the standard output.
+    ///
+    /// - parameter messages:   Messages to write to standard output.
+    /// - parameter condition:  To achieve assert like behavior you can pass condition that must be met before log.
+    /// - parameter separator:  The separator to use between messages. The default value is `" "`.
+    /// - parameter terminator: To print without a trailing newline, pass `terminator: ""`. The default value is `"\n"`.
+    /// - parameter className:  The name of the class where this log is executed. The default value is extracted from `#file`.
+    /// - parameter lineNumber: The line number where this log is executed. The default value is extracted from `#line`.
+    public static func log(messages: Any..., condition: Bool = true, separator: String = " ", terminator: String = "\n", className: String = __FILE__, lineNumber: Int = __LINE__) {
+        guard condition && !disableAllLogs && enableLogLevelDebug else { return }
 
-        if enableLogLevelDebug && !disableAllLogs {
-            print("[\(className):\(lineNumber)] \(message)")
-        }
+        let messages  = messages.map { String($0) }.joinWithSeparator(separator)
+        let className = className.lastPathComponent.stringByDeletingPathExtension
+        print("[\(className):\(lineNumber)]", messages, terminator: terminator)
     }
 
-    public static func info(message: String, className: String = __FILE__, lineNumber: Int = __LINE__) {
-        let className = extractClassName(className)
+    /// Writes the textual representations of info message, separated by separator
+    /// and terminated by terminator, into the standard output.
+    ///
+    /// - parameter messages:   Messages to write to standard output.
+    /// - parameter condition:  To achieve assert like behavior you can pass condition that must be met before log.
+    /// - parameter separator:  The separator to use between messages. The default value is `" "`.
+    /// - parameter terminator: To print without a trailing newline, pass `terminator: ""`. The default value is `"\n"`.
+    /// - parameter className:  The name of the class where this log is executed. The default value is extracted from `#file`.
+    /// - parameter lineNumber: The line number where this log is executed. The default value is extracted from `#line`.
+    public static func info(messages: Any..., condition: Bool = true, separator: String = " ", terminator: String = "\n", className: String = __FILE__, lineNumber: Int = __LINE__) {
+        guard condition && !disableAllLogs && enableLogLevelInfo else { return }
 
-        if enableLogLevelInfo && !disableAllLogs {
-            print("[\(className):\(lineNumber)] \(message)")
-        }
+        let messages  = messages.map { String($0) }.joinWithSeparator(separator)
+        let className = className.lastPathComponent.stringByDeletingPathExtension
+        print("[\(className):\(lineNumber)]", messages, terminator: terminator)
     }
 
-    public static func warn(message: String, className: String = __FILE__, lineNumber: Int = __LINE__) {
-        let className = extractClassName(className)
+    /// Writes the textual representations of warning message, separated by separator
+    /// and terminated by terminator, into the standard output.
+    ///
+    /// - parameter messages:   Messages to write to standard output.
+    /// - parameter condition:  To achieve assert like behavior you can pass condition that must be met before log.
+    /// - parameter separator:  The separator to use between messages. The default value is `" "`.
+    /// - parameter terminator: To print without a trailing newline, pass `terminator: ""`. The default value is `"\n"`.
+    /// - parameter className:  The name of the class where this log is executed. The default value is extracted from `#file`.
+    /// - parameter lineNumber: The line number where this log is executed. The default value is extracted from `#line`.
+    public static func warn(messages: Any..., condition: Bool = true, separator: String = " ", terminator: String = "\n", className: String = __FILE__, lineNumber: Int = __LINE__) {
+        guard condition && !disableAllLogs && enableLogLevelWarn else { return }
 
-        if enableLogLevelWarn && !disableAllLogs {
-            print("[\(className):\(lineNumber)] WARNING: \(message)")
-        }
+        let messages  = messages.map { String($0) }.joinWithSeparator(separator)
+        let className = className.lastPathComponent.stringByDeletingPathExtension
+        print("[\(className):\(lineNumber)] WARNING:", messages, terminator: terminator)
     }
 
-    public static func error(message: String, className: String = __FILE__, lineNumber: Int = __LINE__) {
-        let className = extractClassName(className)
+    /// Writes the textual representations of error message, separated by separator
+    /// and terminated by terminator, into the standard output.
+    ///
+    /// - parameter messages:   Messages to write to standard output.
+    /// - parameter condition:  To achieve assert like behavior you can pass condition that must be met before log.
+    /// - parameter separator:  The separator to use between messages. The default value is `" "`.
+    /// - parameter terminator: To print without a trailing newline, pass `terminator: ""`. The default value is `"\n"`.
+    /// - parameter className:  The name of the class where this log is executed. The default value is extracted from `#file`.
+    /// - parameter lineNumber: The line number where this log is executed. The default value is extracted from `#line`.
+    public static func error(messages: Any..., condition: Bool = true, separator: String = " ", terminator: String = "\n", className: String = __FILE__, lineNumber: Int = __LINE__) {
+        guard condition && !disableAllLogs && enableLogLevelError else { return }
 
-        if enableLogLevelError && !disableAllLogs {
-            print("[\(className):\(lineNumber)] ERROR: \(message)")
-        }
-    }
-
-    private static func extractClassName(className: String) -> String {
-      return ((className as NSString).lastPathComponent as NSString).stringByDeletingPathExtension
+        let messages  = messages.map { String($0) }.joinWithSeparator(separator)
+        let className = className.lastPathComponent.stringByDeletingPathExtension
+        print("[\(className):\(lineNumber)] ERROR:", messages, terminator: terminator)
     }
 }
