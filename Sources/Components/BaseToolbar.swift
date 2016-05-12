17ebaf54bc704a50f2db759f58d9a2043773fa72
Zeeshan Mian <zmian@me.com>
2016-05-11 21:25:56 -0400

    Added BaseToolbar

---
+++ Sources/Components/BaseToolbar.swift
@@ -0,0 +1,72 @@
+//
+// BaseToolbar.swift
+//
+// Copyright © 2016 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+public class BaseToolbar: UIToolbar {
+    /// The default value is `44` (system's standard).
+    public var preferredHeight: CGFloat = 44
+
+    // MARK: Init Methods
+
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    // MARK: Setup Methods
+
+    private func commonInit() {
+        setupSubviews()
+    }
+
+    /// The default implementation of this method does nothing.
+    ///
+    /// Subclasses can override it to perform additional actions,
+    /// for example, add new subviews or configure properties.
+    /// This method is called when self is initialized using any of the relevant `init` methods.
+    public func setupSubviews() {}
+
+    // MARK: Override Methods
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        frame.size.height = preferredHeight
+    }
+
+    public override func sizeThatFits(size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height = preferredHeight
+        return size
+    }
+}
