fb40dec631563b6cbf584585325179a2829b5425
Zeeshan Mian <zmian@me.com>
2017-08-16 11:58:31 -0400

    Added Swizzle function

---
+++ Sources/Classes/Swizzle.swift
@@ -0,0 +1,77 @@
+//
+// Swizzle.swift
+//
+// Copyright © 2017 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+/// Swizzle selector for the given class.
+///
+/// ```swift
+/// extension UIApplication {
+///     private static let runOnce: Void = {
+///         UIViewController.runOnceSwapViewWillAppear()
+///     }()
+///
+///     override open var next: UIResponder? {
+///         // Called before applicationDidFinishLaunching
+///         UIApplication.runOnce
+///         return super.next
+///     }
+/// }
+///
+/// extension UIViewController {
+///     fileprivate static func runOnceSwapViewWillAppear() {
+///         swizzle(
+///             UIViewController.self,
+///             originalSelector: #selector(UIViewController.viewWillAppear(_:)),
+///             swizzledSelector: #selector(UIViewController.swizzled_viewWillAppear(_:))
+///         )
+///     }
+///
+///     @objc fileprivate func swizzled_viewWillAppear(_ animated: Bool) {
+///         self.swizzled_viewWillAppear(animated)
+///     }
+/// }
+/// ```
+public func swizzle(_ forClass: AnyClass, originalSelector: Selector, swizzledSelector: Selector) {
+    let originalMethod = class_getInstanceMethod(forClass, originalSelector)
+    let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector)
+
+    let didAddMethod = class_addMethod(
+        forClass,
+        originalSelector,
+        method_getImplementation(swizzledMethod),
+        method_getTypeEncoding(swizzledMethod)
+    )
+
+    if didAddMethod {
+        class_replaceMethod(
+            forClass,
+            swizzledSelector,
+            method_getImplementation(originalMethod),
+            method_getTypeEncoding(originalMethod)
+        )
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod)
+    }
+}
