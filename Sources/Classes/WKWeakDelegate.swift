2dc486e6e1984790576b3b1bfe86c51dde75146c
Zeeshan Mian <zmian@me.com>
2017-08-17 19:52:53 -0400

    Added WKWeakDelegate to break retain cycle

---
+++ Sources/Classes/WKWeakDelegate.swift
@@ -0,0 +1,57 @@
+//
+// WKWeakDelegate.swift
+//
+// Copyright © 2017 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+import WebKit
+
+/// `WKUserContentController` retains `WKScriptMessageHandler` so this object
+/// exists to break that retain cycle.
+fileprivate class WKWeakDelegate: NSObject, WKScriptMessageHandler {
+    fileprivate weak var delegate: WKScriptMessageHandler?
+
+    init(_ delegate: WKScriptMessageHandler) {
+        self.delegate = delegate
+        super.init()
+    }
+
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        delegate?.userContentController(userContentController, didReceive: message)
+    }
+}
+
+// MARK: Swizzle
+
+extension WKUserContentController {
+    static func runOnceSwapSelectors() {
+        swizzle(
+            WKUserContentController.self,
+            originalSelector: #selector(WKUserContentController.add(_:name:)),
+            swizzledSelector: #selector(WKUserContentController.swizzled_add(_:name:))
+        )
+    }
+
+    @objc fileprivate func swizzled_add(_ scriptMessageHandler: WKScriptMessageHandler, name: String) {
+        self.swizzled_add(WKWeakDelegate(scriptMessageHandler), name: name)
+    }
+}
