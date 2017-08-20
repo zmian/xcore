f22ff02a6cb2673c665a150e09c2eb3ca96e65fa
Zeeshan Mian <zmian@me.com>
2017-08-20 00:13:01 -0400

    Added Type Based Constants

---
+++ Sources/Helpers/Constants.swift
@@ -0,0 +1,60 @@
+//
+// Constants.swift
+//
+// Copyright © 2017 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// MARK: Type Based Constants
+
+extension TimeInterval {
+    /// The fast duration to use for animations when the desired interval is between `0.2...0.3` seconds.
+    ///
+    /// ```swift
+    /// UIView.animate(withDuration: .fast) {
+    ///     ...
+    /// }
+    /// ```
+    public static var fast: TimeInterval {
+        return 0.25
+    }
+
+    /// The normal duration to use for animations when the desired interval is between `0.3...0.5` seconds.
+    ///
+    /// ```swift
+    /// UIView.animate(withDuration: .normal) {
+    ///     ...
+    /// }
+    /// ```
+    public static var normal: TimeInterval {
+        return 0.35
+    }
+
+    /// The slow duration to use for animations when the desired interval is between `> 0.5` seconds.
+    ///
+    /// ```swift
+    /// UIView.animate(withDuration: .slow) {
+    ///     ...
+    /// }
+    /// ```
+    public static var slow: TimeInterval {
+        return 0.5
+    }
+}
