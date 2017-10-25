2ab45d5d19387ef9b61eba7c2068456e13285e45
Zeeshan Mian <zmian@me.com>
2017-10-25 15:43:22 -0400

    Added UIDevice+Biometrics

---
+++ Sources/Extensions/UIKit/UIDevice+Biometrics.swift
@@ -0,0 +1,60 @@
+//
+// UIDevice+Biometrics.swift
+//
+// Copyright © 2017 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+import LocalAuthentication
+
+extension UIDevice {
+    /// Indicates that the device owner can authenticate using biometry, Touch ID or Face ID.
+    public var hasBiometricsSupport: Bool {
+        return LAContext().canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+    }
+
+    /// The name of the biometry authentication. "Touch ID" or "Face ID"; otherwise, an empty string.
+    public var biometryName: String {
+        if #available(iOS 11.0, *) {
+            switch biometryType {
+                case .none:
+                    return ""
+                case .typeTouchID:
+                    return "Touch ID"
+                case .typeFaceID:
+                    return "Face ID"
+            }
+        } else {
+            guard hasBiometricsSupport else { return "" }
+            return "Touch ID"
+        }
+    }
+
+    /// The name of the biometry type authentication. "Touch ID" or "Face ID"; otherwise, an empty string.
+    @available(iOS 11.0, *)
+    public var biometryType: LABiometryType {
+        let context = LAContext()
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) else {
+            return .none
+        }
+        return context.biometryType
+    }
+}
