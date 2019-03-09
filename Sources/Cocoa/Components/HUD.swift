81d43e4d62df10c8506fa47b8a7f8e8145a249ff
Zeeshan Mian <zmian@me.com>
2019-03-08 20:01:25 -0500

    Added HUD and Count type and extended associated type to include the automatically save the default value using a policy.

---
+++ Sources/Cocoa/Components/HUD.swift
@@ -0,0 +1,154 @@
+//
+// HUD.swift
+//
+// Copyright © 2019 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+extension HUD {
+    public struct Duration {
+        public let show: TimeInterval
+        public let hide: TimeInterval
+
+        public init(_ duration: TimeInterval) {
+            self.show = duration
+            self.hide = duration
+        }
+
+        public init(show: TimeInterval, hide: TimeInterval) {
+            self.show = show
+            self.hide = hide
+        }
+
+        public static var `default`: Duration {
+            return Duration(.normal)
+        }
+    }
+}
+
+/// A base class to create a HUD that sets up blank canvas that can be
+/// customized by subclasses to show anything in a fullscreen window.
+open class HUD {
+    private let window = UIWindow(frame: UIScreen.main.bounds)
+    private let view = UIView()
+    private var isEnabled = false
+    private var temporaryUnavailable = false
+
+    /// The default value is `.white`.
+    open var backgroundColor: UIColor = .white {
+        didSet {
+            view.backgroundColor = backgroundColor
+        }
+    }
+
+    /// The default value is `.normal`.
+    open var duration: Duration {
+        return .default
+    }
+
+    open var windowLevel: UIWindow.Level {
+        get { return window.windowLevel }
+        set { window.windowLevel = newValue }
+    }
+
+    public init() {
+        window.backgroundColor = .clear
+        window.addSubview(view)
+        view.backgroundColor = .white
+        NSLayoutConstraint.constraintsForViewToFillSuperview(view).activate()
+    }
+
+    open func moveWindowToFront() {
+        let windowLevel = UIApplication.sharedOrNil?.windows.last?.windowLevel ?? .normal
+        let maxWinLevel = max(windowLevel, .normal)
+        window.windowLevel = maxWinLevel + 1
+    }
+
+    open func addSubview(_ view: UIView) {
+        self.view.addSubview(view)
+    }
+
+    private func toggle(enabled: Bool, animated: Bool, _ completion: (() -> Void)?) {
+        guard isEnabled != enabled, !temporaryUnavailable else {
+            temporaryUnavailable = false
+            completion?()
+            return
+        }
+
+        let duration = enabled ? self.duration.show : self.duration.hide
+
+        isEnabled = enabled
+        moveWindowToFront()
+        if enabled {
+            window.isHidden = false
+            window.makeKey()
+
+            guard animated else {
+                view.alpha = 1
+                completion?()
+                return
+            }
+
+            view.alpha = 0
+            UIView.animate(withDuration: duration, animations: {
+                self.view.alpha = 1
+            }, completion: { _ in
+                completion?()
+            })
+        } else {
+            guard animated else {
+                view.alpha = 0
+                window.isHidden = true
+                completion?()
+                return
+            }
+            UIView.animate(withDuration: duration, animations: {
+                self.view.alpha = 0
+            }, completion: { _ in
+                self.window.isHidden = true
+                completion?()
+            })
+        }
+    }
+
+    private func toggle(enabled: Bool, delay delayDuration: TimeInterval, animated: Bool, _ completion: (() -> Void)?) {
+        guard delayDuration > 0 else {
+            return toggle(enabled: enabled, animated: animated, completion)
+        }
+
+        delay(by: delayDuration) { [weak self] in
+            self?.toggle(enabled: enabled, animated: animated, completion)
+        }
+    }
+
+    open func show(delay delayDuration: TimeInterval = 0, animated: Bool = true, _ completion: (() -> Void)? = nil) {
+        toggle(enabled: true, delay: delayDuration, animated: animated, completion)
+    }
+
+    open func hide(delay delayDuration: TimeInterval = 0, animated: Bool = true, _ completion: (() -> Void)? = nil) {
+        toggle(enabled: false, delay: delayDuration, animated: animated, completion)
+    }
+
+    open func disableOnNextCall() {
+        temporaryUnavailable = true
+    }
+}
---
+++ Sources/Swift/Components/Count.swift
@@ -0,0 +1,61 @@
+//
+// Count.swift
+//
+// Copyright © 2019 Zeeshan Mian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import Foundation
+
+/// A type that can be used to represent a finite or infinite count.
+///
+/// # Example Usage
+///
+/// ```swift
+/// var count: Count = .infinite
+/// // print(count) // infinite
+///
+/// var count: Count = 1
+/// // print(count) // 1
+///
+/// count -= 1
+/// // print(count) // 0
+///
+/// if count == 0 {
+///     print("Remaining count is 0.")
+/// }
+/// ```
+public enum Count: Equatable, ExpressibleByIntegerLiteral, CustomStringConvertible {
+    case infinite
+    case times(Int)
+
+    public init(integerLiteral value: Int) {
+        self = .times(value)
+    }
+
+    public var description: String {
+        switch self {
+        case .infinite:
+            return "infinite"
+        case .times(let count):
+            return "\(count)"
+        }
+    }
+}
