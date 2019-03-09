17b88bcb8405df18381ea3133dade5de45c7a59d
Zeeshan Mian <zmian@me.com>
2019-03-08 20:01:25 -0500

    Added HUD and Count type and extended associated type to include the automatically save the default value using a policy.

--- Sources/Cocoa/Protocols/ExtensibleByAssociatedObject.swift
+++ Sources/Cocoa/Protocols/ExtensibleByAssociatedObject.swift
@@ -40,11 +40,20 @@ extension ExtensibleByAssociatedObject {
     /// - Parameters:
     ///   - key: The key for the association.
     ///   - defaultValue: The default value to return if the no associated value is found.
+    ///   - defaultValueAssociationPolicy: An optional value to save the `defaultValue` so the next call
+    ///                                    will have the associated object.
     /// - Returns: The value associated with the key for object.
-    public func associatedObject<T>(_ key: UnsafeRawPointer, default defaultValue: @autoclosure () -> T) -> T {
+    public func associatedObject<T>(_ key: UnsafeRawPointer, default defaultValue: @autoclosure () -> T, policy defaultValueAssociationPolicy: AssociatedObjectPolicy? = nil) -> T {
         guard let value = objc_getAssociatedObject(self, key) as? T else {
-            return defaultValue()
+            let defaultValue = defaultValue()
+
+            if let associationPolicy = defaultValueAssociationPolicy {
+                setAssociatedObject(key, value: defaultValue, policy: associationPolicy)
+            }
+
+            return defaultValue
         }
+
         return value
     }
 
@@ -53,9 +62,49 @@ extension ExtensibleByAssociatedObject {
     /// - Parameters:
     ///   - key: The key for the association.
     ///   - value: The value to associate with the key for object. Pass `nil` to clear an existing association.
-    ///   - policy: The policy for the association. The default value is `.OBJC_ASSOCIATION_RETAIN_NONATOMIC`.
-    public func setAssociatedObject<T>(_ key: UnsafeRawPointer, value: T?, policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC) {
-        objc_setAssociatedObject(self, key, value, policy)
+    ///   - policy: The policy for the association. The default value is `.strong`.
+    public func setAssociatedObject<T>(_ key: UnsafeRawPointer, value: T?, policy: AssociatedObjectPolicy = .strong) {
+        objc_setAssociatedObject(self, key, value, policy.rawValue)
+    }
+}
+
+public enum AssociatedObjectPolicy {
+    /// Specifies a weak reference to the associated object.
+    case weak
+
+    /// Specifies a strong reference to the associated object.
+    ///
+    /// The association is not made atomically.
+    case strong
+
+    /// Specifies a strong reference to the associated object.
+    ///
+    /// The association is made atomically.
+    case strongAtomic
+
+    /// Specifies that the associated object is copied.
+    ///
+    /// The association is not made atomically.
+    case copy
+
+    /// Specifies that the associated object is copied.
+    ///
+    /// The association is made atomically.
+    case copyAtomic
+
+    fileprivate var rawValue: objc_AssociationPolicy {
+        switch self {
+            case .weak:
+                return .OBJC_ASSOCIATION_ASSIGN
+            case .strong:
+                return .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+            case .strongAtomic:
+                return .OBJC_ASSOCIATION_RETAIN
+            case .copy:
+                return .OBJC_ASSOCIATION_COPY_NONATOMIC
+            case .copyAtomic:
+                return .OBJC_ASSOCIATION_COPY
+        }
     }
 }
 
