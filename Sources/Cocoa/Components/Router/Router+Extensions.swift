0d05496e5afc8959b7fbfda95b36653305e5fc9f
Zeeshan Mian <zmian@me.com>
2019-07-04 15:57:01 -0400

    Moved Router objects under Router namespace to avoid global namespace pollution.

---
+++ Sources/Cocoa/Components/Router/Router+Extensions.swift
@@ -0,0 +1,99 @@
+//
+// Router+Extensions.swift
+//
+// Copyright © 2019 Xcore
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+extension UINavigationController {
+    private struct AssociatedKey {
+        static var router = "router"
+    }
+
+    fileprivate var _navigationControllerRouter: Router {
+        get {
+            let router: Router
+
+            if let existingRouter: Router = associatedObject(&AssociatedKey.router) {
+                router = existingRouter
+            } else {
+                router = Router(navigationController: self)
+                self._navigationControllerRouter = router
+            }
+
+            return router
+        }
+        set { setAssociatedObject(&AssociatedKey.router, value: newValue) }
+    }
+}
+
+extension UIViewController {
+    public var router: Router {
+        guard let navigationController = navigationController else {
+            if let navController = self as? UINavigationController {
+                return navController._navigationControllerRouter
+            }
+
+            fatalError("Router requires a navigation controller.")
+        }
+
+        return navigationController._navigationControllerRouter
+    }
+}
+
+extension XCCollectionViewDataSource {
+    public var router: Router {
+        guard let collectionView = collectionView else {
+            return Router(navigationController: nil)
+        }
+
+        guard let router = collectionView.viewController?.router else {
+            #if DEBUG
+            if isDebuggerAttached {
+                fatalError("Datasource doesn't have a view controller.")
+            }
+            #endif
+            return Router(navigationController: nil)
+        }
+
+        return router
+    }
+}
+
+extension XCTableViewDataSource {
+    public var router: Router {
+        guard let tableView = tableView else {
+            return Router(navigationController: nil)
+        }
+
+        guard let router = tableView.viewController?.router else {
+            #if DEBUG
+            if isDebuggerAttached {
+                fatalError("Datasource doesn't have a view controller.")
+            }
+            #endif
+            return Router(navigationController: nil)
+        }
+
+        return router
+    }
+}
---
+++ Sources/Cocoa/Components/Router/Router+Route.swift
@@ -0,0 +1,121 @@
+//
+// Router+Route.swift
+//
+// Copyright © 2019 Xcore
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+extension Router {
+    /// A routes configuration.
+    ///
+    /// Simple and powerful way to create multiple routers and navigate from any
+    /// where.
+    ///
+    /// `UIViewController` has a router property (`UIViewController.router`) which
+    /// should be used to navigate to routes.
+    ///
+    /// **Routes Declaration**
+    ///
+    /// ```swift
+    /// final class AuthenticationRouter: RouteHandler { }
+    ///
+    /// extension Router.Route where Type == AuthenticationRouter {
+    ///     static var login: Router.Route {
+    ///         return .init(LoginViewController())
+    ///     }
+    /// }
+    ///
+    /// final class MainRouter: RouteHandler { }
+    ///
+    /// extension Router.Route where Type == MainRouter {
+    ///     static var home: Router.Route {
+    ///         return .init(HomeViewController())
+    ///     }
+    ///
+    ///     static var profile(user: User) -> Router.Route {
+    ///         return .init(ProfileViewController(user: user))
+    ///     }
+    ///
+    ///     static var likes(user: User) -> Router.Route {
+    ///         return .init { router in
+    ///             return LikesViewController(user: user).apply {
+    ///                 $0.didTapOnProfile {
+    ///                    router.route(to: .profile(user: user))
+    ///                 }
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// **Register the child routers with the parent Router**
+    ///
+    /// ```
+    /// extension Router {
+    ///     var main: MainRouter {
+    ///         return register(MainRouter())
+    ///     }
+    ///
+    ///    var auth: AuthenticationRouter {
+    ///        return register(AuthenticationRouter())
+    ///    }
+    /// }
+    /// ```
+    ///
+    /// **Usage**
+    ///
+    /// ```swift
+    /// final class HomeViewController: UIViewController {
+    ///     private let user: User
+    ///
+    ///     private func showProfile() {
+    ///         router.main.route(to: .profile(user: user))
+    ///     }
+    ///
+    ///     private func showLogin() {
+    ///         router.auth.route(to: .login)
+    ///     }
+    /// }
+    /// ```
+    public struct Route<Type: RouteHandler> {
+        public var identifier: String
+        public var configure: (Type) -> RouteKind
+
+        public init(identifier: String? = nil, _ configure: @escaping ((Type) -> RouteKind)) {
+            self.identifier = identifier ?? "___defaultIdentifier___"
+            self.configure = configure
+        }
+
+        public init(identifier: String? = nil, _ configure: @escaping ((Type) -> UIViewController)) {
+            self.identifier = identifier ?? "___defaultIdentifier___"
+            self.configure = { router -> RouteKind in
+                .viewController(configure(router))
+            }
+        }
+
+        public init(_ configure: @escaping @autoclosure () -> UIViewController) {
+            self.init { _ -> RouteKind in
+                .viewController(configure())
+            }
+        }
+    }
+}
