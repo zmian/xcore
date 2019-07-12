730706d67c3eb00684f30dce606f5b735b41d0a8
Zeeshan Mian <zmian@me.com>
2019-07-12 13:43:35 -0400

    Added group route.

--- Sources/Cocoa/Extensions/UINavigationController+Extensions.swift
+++ Sources/Cocoa/Extensions/UINavigationController+Extensions.swift
@@ -75,6 +75,33 @@ extension UINavigationController {
         }, completionHandler: completion)
     }
 
+    /// Pushes a list of view controller onto the receiver’s stack and updates the display.
+    ///
+    /// The object in the viewController parameter becomes the top view controller
+    /// on the navigation stack. Pushing a view controller causes its view to be
+    /// embedded in the navigation interface. If the animated parameter is `true`, the
+    /// view is animated into position; otherwise, the view is simply displayed in
+    /// its final location.
+    ///
+    /// In addition to displaying the view associated with the new view controller at
+    /// the top of the stack, this method also updates the navigation bar and tool bar
+    /// accordingly. For information on how the navigation bar is updated.
+    ///
+    /// - Parameters:
+    ///   - viewControllers: The view controller to push onto the stack. This object cannot be a tab bar controller.
+    ///                     If the view controller is already on the navigation stack, this method throws an exception.
+    ///   - animated: Specify `true` to animate the transition or `false` if you do not want the transition to be animated.
+    ///               You might specify `false` if you are setting up the navigation controller at launch time.
+    ///   - completion: The block to execute after the presentation finishes.
+    open func pushViewController(_ viewControllers: [UIViewController], animated: Bool, completion: (() -> Void)? = nil) {
+        var vcs = self.viewControllers
+        vcs.append(contentsOf: viewControllers)
+
+        CATransaction.animationTransaction({
+            setViewControllers(vcs, animated: animated)
+        }, completionHandler: completion)
+    }
+
     /// A convenience method to pop to view controller of specified subclass of `UIViewController` type.
     ///
     /// - Parameters:
