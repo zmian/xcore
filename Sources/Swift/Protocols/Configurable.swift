47033099fbc96da7cfffb746defb4e7494d24ed6
Zeeshan Mian <zmian@me.com>
2019-08-04 20:31:40 -0400

    Added AnyConfigurable

--- Sources/Swift/Protocols/Configurable.swift
+++ Sources/Swift/Protocols/Configurable.swift
@@ -26,3 +26,23 @@ public protocol Configurable {
     associatedtype ObjectType
     func configure(_ object: ObjectType)
 }
+
+// MARK: - AnyConfigurable
+
+public struct AnyConfigurable<T> {
+    public let base: Any
+    private let _configure: (_ object: T) -> Void
+
+    public init<C: Configurable>(_ base: C) where C.ObjectType == T {
+        self.base = base
+        self._configure = {
+            base.configure($0)
+        }
+    }
+}
+
+extension AnyConfigurable: Configurable {
+    public func configure(_ object: T) {
+        _configure(object)
+    }
+}
