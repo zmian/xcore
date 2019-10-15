8485a01238976bf2ce874fac91f88b8b65727c1f
Zeeshan Mian <zmian@me.com>
2019-10-15 00:02:38 -0400

    Fixed the issue with mutable appliable

--- Example/Helpers/Theme.swift
+++ Example/Helpers/Theme.swift
@@ -29,6 +29,22 @@ extension Theme {
             $0.height = AppConstants.uiControlsHeight
             $0.isHeightSetAutomatically = true
             $0.highlightedAnimation = .scale
+            $0.configurationAttributes.apply {
+                // Styles Updates
+                $0[.base].font = .app(style: .body)
+                $0[.base].textColor = current.buttonTextColor
+                $0[.base].tintColor = current.tintColor
+
+                $0[.callout].textColor = .white
+                $0[.callout].backgroundColor = .orange
+                $0[.calloutSecondary].backgroundColor = .green
+                $0[.pill].backgroundColor = .gray
+
+                // Toggle Styles
+                $0[.checkbox].font = .app(style: .caption2)
+                $0[.checkbox].tintColor = current.toggleColor
+                $0[.radioButton].tintColor = current.toggleColor
+            }
         }
 
         MarkupText.appearance.apply {
