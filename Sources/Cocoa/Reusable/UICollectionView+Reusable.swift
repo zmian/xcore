1115f3fa215bcb0422c2028c6d1fb60e2a5e4f3a
Zeeshan Mian <zmian@me.com>
2019-10-30 10:31:40 -0400

    Fixed comment typo.

--- Sources/Cocoa/Reusable/UICollectionView+Reusable.swift
+++ Sources/Cocoa/Reusable/UICollectionView+Reusable.swift
@@ -96,10 +96,12 @@ extension UICollectionView {
 }
 
 extension UICollectionView {
-    /// Returns wether an index path is valid for the current layout information.
+    /// Returns whether the given index path is valid for the current layout
+    /// information.
     open func isValid(indexPath: IndexPath) -> Bool {
         return indexPath.section < numberOfSections && indexPath.row < numberOfItems(inSection: indexPath.section)
     }
+
     /// Returns the layout information for the specified supplementary view.
     ///
     /// Use this method to retrieve the layout information for a particular
@@ -120,6 +122,7 @@ extension UICollectionView {
         guard isValid(indexPath: indexPath) else {
             return nil
         }
+
         return layoutAttributesForSupplementaryElement(ofKind: kind.rawValue, at: indexPath)
     }
 
