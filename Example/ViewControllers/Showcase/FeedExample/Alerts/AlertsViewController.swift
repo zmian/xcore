bb4bf3cf309b215f32981c06c03fc14353c5a26e
Guillermo Waitzel <guillermo@toptal.com>
2019-12-03 18:56:13 -0300

    Stacking Data Source

--- Example/ViewControllers/Showcase/FeedExample/Alerts/AlertsViewController.swift
+++ Example/ViewControllers/Showcase/FeedExample/Alerts/AlertsViewController.swift
@@ -65,18 +65,107 @@ final class AlertsViewController: XCComposedCollectionViewController {
         for i in 0...2 {
             allDataSources.append(FeedDataSource(collectionView: collectionView, sectionIndex: i))
         }
-        allDataSources.append(AlertDataSource(collectionView: collectionView, alerts: twoAlerts, identifier: "FirstAlerts"))
+        allDataSources.append(alertsDataSource(
+            viewModel: AlertsViewModel(
+                identifier: "FirstAlerts",
+                alerts: severalAlerts
+            )
+        ))
 
         for i in 3...6 {
             allDataSources.append(FeedDataSource(collectionView: collectionView, sectionIndex: i))
         }
-        allDataSources.append(AlertDataSource(collectionView: collectionView, alerts: severalAlerts, identifier: "SecondAlerts"))
+        allDataSources.append(alertsDataSource(
+            viewModel: AlertsViewModel(
+                identifier: "SecondAlerts",
+                alerts: manyAlerts
+            )
+        ))
 
         for i in 7...10 {
             allDataSources.append(FeedDataSource(collectionView: collectionView, sectionIndex: i))
         }
-        allDataSources.append(AlertDataSource(collectionView: collectionView, alerts: manyAlerts, identifier: "ThirdAlerts"))
+        allDataSources.append(alertsDataSource(
+            viewModel: AlertsViewModel(
+                identifier: "ThirdAlerts",
+                composedAlerts: [twoAlerts, twoAlerts]
+            )
+        ))
 
         return allDataSources
+    }
+
+    private func alertsDataSource(viewModel: AlertsViewModel) -> StackingDataSource {
+        let cellProvider: StackingDataSource.CellProvider = { collectionView, indexPath, item in
+            let cell = collectionView.dequeueReusableCell(for: indexPath) as FeedTextViewCell
+            guard let alertMessage = item as? String else {
+                return cell
+            }
+            cell.configure(
+                title: "\(indexPath.section)",
+                subtitle: alertMessage
+            )
+            return cell
+        }
+        return StackingDataSource(
+            collectionView: collectionView,
+            viewModel: viewModel,
+            cellProvider: cellProvider
+        )
+    }
+}
+
+private class AlertsViewModel: StackingDataSourceViewModel {
+    var tileIdentifier: String = ""
+    var composedAlerts: [[String]]
+
+    convenience init(identifier: String, alerts: [String]) {
+        self.init(identifier: identifier, composedAlerts: alerts.map { [$0] })
+    }
+
+    init(identifier: String, composedAlerts: [[String]]) {
+        self.tileIdentifier = identifier
+        self.composedAlerts = composedAlerts
+    }
+
+    public var numberOfSections: Int {
+        return composedAlerts.count
+    }
+
+    public func itemsCount(for section: Int) -> Int {
+        return composedAlerts.at(section)?.count ?? 0
+    }
+
+    func item(at index: IndexPath) -> Any? {
+        composedAlerts.at(index.section)?.at(index.row)
+    }
+
+    private var didChange: (() -> Void)?
+    func didChange(_ callback: @escaping () -> Void) {
+        didChange = callback
+    }
+
+    func didTap(at index: IndexPath) {
+    }
+
+    var isShadowEnabled: Bool {
+        return true
+    }
+
+    var isClearButtonHidden: Bool {
+        isEmpty
+    }
+
+    func clearAll() {
+        composedAlerts.removeAll()
+        didChange?()
+    }
+
+    var clearButtonTitle: String {
+        "Clear"
+    }
+
+    var showLessButtonTitle: String {
+        "Show Less"
     }
 }
