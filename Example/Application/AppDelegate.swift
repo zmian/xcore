fe94b022a3c92bc06de4edf26384592ccbabe0a6
Zeeshan Mian <zmian@me.com>
2020-06-16 21:57:31 -0400

    Fixed the issue where Swizzle wan't working

--- Example/Application/AppDelegate.swift
+++ Example/Application/AppDelegate.swift
@@ -11,6 +11,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UIApplication.swizzle()
         Theme.start()
         return true
     }
