zmian/xcore

commit 6fb51a9d0fde4f22501ce4015b44d91fd6dcbf20

Accessibility updates to LabelTextView and UILabel (#63)
Add text style to font descriptor when using app method override accessibility traits on label text view.

Sources/Cocoa/Extensions/UIFont/UIFont+App.swift
@@ -71,11 +71,11 @@ extension UIFont {
         style: UIFont.TextStyle,
         compatibleWith traitCollection: UITraitCollection? = nil
     ) -> UIFont {
-        let preferredPointSize = UIFontDescriptor.preferredFontDescriptor(
+        let fontDescriptor = UIFontDescriptor.preferredFontDescriptor(
             withTextStyle: style,
             compatibleWith: traitCollection
-        ).pointSize
+        ).addingAttributes([.name: name])
 
-        return UIFont(name: name, size: preferredPointSize)!
+        return UIFont(descriptor: fontDescriptor, size: fontDescriptor.pointSize)
     }
 }

Sources/Cocoa/Extensions/UIFont/UIFont+Extensions.swift
@@ -81,6 +81,12 @@ extension UIFont {
     }
 }
 
+extension UIFont {
+    public var textStyle: UIFont.TextStyle? {
+        self.fontDescriptor.object(forKey: .textStyle) as? UIFont.TextStyle
+    }
+}
+
 extension UIFont.TextStyle: CaseIterable {
     public static var allCases: [UIFont.TextStyle] = {
         [

Sources/Cocoa/Components/LabelTextView.swift
@@ -41,6 +41,7 @@ open class LabelTextView: UITextView {
         #if canImport(Haring)
         isAccessibilityRotorHintEnabled = true
         #endif
+        isAccessibilityElement = true
         dataDetectorTypes = .all
         textContainerInset = .zero
         textContainer.lineFragmentPadding = 0
@@ -139,5 +140,25 @@ extension LabelTextView {
     @objc open override dynamic var linkTextAttributes: [NSAttributedString.Key: Any]! {
         get { super.linkTextAttributes }
         set { super.linkTextAttributes = newValue }
+    }
+}
+
+// MARK: - Accessibility
+
+extension LabelTextView {
+    /// The accessibility trait value that the element takes based on the text style
+    /// it was given when created.
+     open override var accessibilityTraits: UIAccessibilityTraits {
+        get {
+            guard
+                let currentFont = font,
+                let textStyle = currentFont.textStyle
+            else {
+                return super.accessibilityTraits
+            }
+
+            return textStyle.isTitle ? .header : super.accessibilityTraits
+        }
+        set { super.accessibilityTraits = newValue }
     }
 }

Sources/Cocoa/Extensions/UILabel+Extensions.swift
@@ -149,3 +149,18 @@ extension UILabel {
         )
     }
 }
+
+// MARK: - Accessibility
+
+extension UILabel {
+    /// The accessibility trait value that the element takes based on the text style
+    /// it was given when created.
+    open override var accessibilityTraits: UIAccessibilityTraits {
+        get {
+            guard let textStyle = font.textStyle else { return super.accessibilityTraits }
+
+            return textStyle.isTitle ? .header : super.accessibilityTraits
+        }
+        set { super.accessibilityTraits = newValue }
+    }
+}

commit 1f80bf7aca02c98b7b8bef98a92c29ceb4639ddc

Fixed a regression introduced with #63, the issue with Font descriptor ignore the custom font and always defaulting to system font. Adding text style to the font descriptor causes the system font to take over.

Sources/Cocoa/Extensions/UIFont/UIFont+App.swift
@@ -71,11 +71,13 @@ extension UIFont {
         style: UIFont.TextStyle,
         compatibleWith traitCollection: UITraitCollection? = nil
     ) -> UIFont {
-        let fontDescriptor = UIFontDescriptor.preferredFontDescriptor(
+        let preferredPointSize = UIFontDescriptor.preferredFontDescriptor(
             withTextStyle: style,
             compatibleWith: traitCollection
-        ).addingAttributes([.name: name])
+        ).pointSize
 
-        return UIFont(descriptor: fontDescriptor, size: fontDescriptor.pointSize)
+        return UIFont(name: name, size: preferredPointSize)!.apply {
+            $0._textStyle = style
+        }
     }
 }

Sources/Cocoa/Extensions/UIFont/UIFont+Extensions.swift
@@ -82,8 +82,17 @@ extension UIFont {
 }
 
 extension UIFont {
+    private struct AssociatedKey {
+        static var textStyle = "textStyle"
+    }
+
+    var _textStyle: UIFont.TextStyle? {
+        get { associatedObject(&AssociatedKey.textStyle) }
+        set { setAssociatedObject(&AssociatedKey.textStyle, value: newValue) }
+    }
+
     public var textStyle: UIFont.TextStyle? {
-        fontDescriptor.object(forKey: .textStyle) as? UIFont.TextStyle
+        _textStyle ?? fontDescriptor.object(forKey: .textStyle) as? UIFont.TextStyle
     }
 }
 