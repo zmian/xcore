7e478a5a13a5d895a93b80fc7646233bf87c5d4c
Zeeshan Mian <zmian@me.com>
2020-08-08 14:45:32 -0400

    Added OptionSet.contains(any:) convenience extension.

---
+++ Sources/Swift/Extensions/OptionSet+Extensions.swift
@@ -0,0 +1,21 @@
+//
+// Xcore
+// Copyright © 2019 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import Foundation
+
+extension OptionSet {
+    /// Returns a Boolean value that indicates whether the set has any members in
+    /// common with the given set.
+    ///
+    /// - Parameter member: A set of the same type as the current set.
+    /// - Returns: `true` if the set has any elements in common with other;
+    ///            otherwise, `false`.
+    ///
+    /// - SeeAlso: `isDisjoint(with:)`
+    public func contains(any member: Self) -> Bool {
+        !isDisjoint(with: member)
+    }
+}
