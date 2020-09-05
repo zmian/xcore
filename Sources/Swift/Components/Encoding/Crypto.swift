82bd9321c30bacf586005148e324f9092d8629fd
Zeeshan Mian <zmian@me.com>
2020-09-04 22:53:36 -0400

    Cleanup: Removed @available(iOS 13.0, *) annotations.

--- Sources/Swift/Components/Encoding/Crypto.swift
+++ Sources/Swift/Components/Encoding/Crypto.swift
@@ -5,12 +5,8 @@
 //
 
 import Foundation
-import CommonCrypto
-#if canImport(CryptoKit)
 import CryptoKit
-#endif
 
-@available(iOS 13.0, *)
 extension Digest {
     public var data: Data {
         Data(makeIterator())
@@ -23,31 +19,11 @@ extension Digest {
 
 extension Data {
     public func md5() -> String {
-        if #available(iOS 13.0, *) {
-            return Insecure.MD5.hash(data: self).hexEncodedString()
-        } else {
-            let hash = withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-                var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-                CC_MD5(bytes.baseAddress, CC_LONG(count), &hash)
-                return hash
-            }
-
-            return hash.hexEncodedString()
-        }
+        Insecure.MD5.hash(data: self).hexEncodedString()
     }
 
     public func sha256() -> String {
-        if #available(iOS 13.0, *) {
-            return SHA256.hash(data: self).hexEncodedString()
-        } else {
-            let hash = withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-                var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-                CC_SHA256(bytes.baseAddress, CC_LONG(count), &hash)
-                return hash
-            }
-
-            return hash.hexEncodedString()
-        }
+        SHA256.hash(data: self).hexEncodedString()
     }
 }
 
