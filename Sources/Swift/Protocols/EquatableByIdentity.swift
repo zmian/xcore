20ef3aafc490c1614d3e9d332899c2d9dfb538b4
Zeeshan Mian <zmian@me.com>
2020-09-17 23:50:28 -0400

    Cleanup: Renamed "EquatableByPointer" to "EquatableByIdentity" and change the implementation to use identity instead of pointers directly.

---
+++ Sources/Swift/Protocols/EquatableByIdentity.swift
@@ -0,0 +1,15 @@
+//
+// Xcore
+// Copyright © 2017 Xcore
+// MIT license, see LICENSE file for details
+//
+
+/// A protocol that auto-implements `Equatable` by using class instance
+/// identity.
+public protocol EquatableByIdentity: AnyObject, Equatable { }
+
+extension EquatableByIdentity {
+    public static func ==(lhs: Self, rhs: Self) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+}
