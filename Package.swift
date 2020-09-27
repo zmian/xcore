85993e5a86f0994467dce315c0db3f63faa11d02
Zeeshan Mian <zmian@me.com>
2020-09-27 11:25:20 -0400

    SPM: Added Package manifest.

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Xcore",
+    products: [
+        .library(name: "Xcore", targets: ["Xcore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.9.1"),
+    ],
+    targets: [
+        .target(name: "Xcore", path: "Sources"),
+        .testTarget(name: "Tests", dependencies: ["Xcore"])
+    ],
+    swiftLanguageVersions: [.v5],
+    platforms: [.iOS(.v13)]
+)
