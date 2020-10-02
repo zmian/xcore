746d90f3784cf5b5d8447628b01f181a000342ad
Zeeshan Mian <zmian@me.com>
2020-10-02 16:48:54 -0400

    HUD: Expose an API to customize the window globally.

--- Sources/Cocoa/Extensions/UITextView+Extensions.swift
+++ Sources/Cocoa/Extensions/UITextView+Extensions.swift
@@ -41,24 +41,29 @@ extension UITextView {
         else {
             return nil
         }
-        return self.textRange(from: start, to: end)
+
+        return textRange(from: start, to: end)
     }
 
-    /// Returns `UIAccessibilityElement` for all links inside current instance
-    /// it was given when created.
+    /// Returns `UIAccessibilityElement` for all links inside current instance it
+    /// was given when created.
     public var linkAccessibilityElements: [UIAccessibilityElement] {
         var linkElements: [UIAccessibilityElement] = []
+
         attributedText.enumerateAttribute(.link, in: NSRange(0..<attributedText.length)) { value, range, _ in
             guard value != nil else {
                 return
             }
+
             let element = UIAccessibilityElement(accessibilityContainer: self)
             element.accessibilityTraits = .link
             element.accessibilityValue = (value as? URL)?.absoluteString
+
             if let textRange = textRange(from: range) {
                 element.accessibilityFrameInContainerSpace = firstRect(for: textRange)
                 element.accessibilityLabel = self.text(in: textRange)
             }
+
             linkElements.append(element)
         }
 
