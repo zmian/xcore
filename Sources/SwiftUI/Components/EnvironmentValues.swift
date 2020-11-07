4cdd7ba1171b79fec7f59b01c74ba20185cd95f3
Zeeshan Mian <zmian@me.com>
2020-11-07 18:08:13 -0500

    EdgeInsets

--- Sources/SwiftUI/Components/EnvironmentValues.swift
+++ Sources/SwiftUI/Components/EnvironmentValues.swift
@@ -8,11 +8,11 @@ import SwiftUI
 
 // MARK: - Theme
 
-private struct ThemeKey: EnvironmentKey {
-    static var defaultValue: Theme = .current
-}
-
 extension EnvironmentValues {
+    private struct ThemeKey: EnvironmentKey {
+        static var defaultValue: Theme = .current
+    }
+
     public var theme: Theme {
         get { self[ThemeKey.self] }
         set { self[ThemeKey.self] = newValue }
@@ -21,11 +21,11 @@ extension EnvironmentValues {
 
 // MARK: - DefaultMinButtonHeight
 
-private struct DefaultMinButtonHeightKey: EnvironmentKey {
-    static var defaultValue: CGFloat = 50
-}
-
 extension EnvironmentValues {
+    private struct DefaultMinButtonHeightKey: EnvironmentKey {
+        static var defaultValue: CGFloat = 50
+    }
+
     public var defaultMinButtonHeight: CGFloat {
         get { self[DefaultMinButtonHeightKey.self] }
         set { self[DefaultMinButtonHeightKey.self] = newValue }
@@ -34,11 +34,11 @@ extension EnvironmentValues {
 
 // MARK: - DefaultButtonCornerRadius
 
-private struct DefaultButtonCornerRadiusKey: EnvironmentKey {
-    static var defaultValue: CGFloat = AppConstants.cornerRadius
-}
-
 extension EnvironmentValues {
+    private struct DefaultButtonCornerRadiusKey: EnvironmentKey {
+        static var defaultValue: CGFloat = AppConstants.cornerRadius
+    }
+
     public var defaultButtonCornerRadius: CGFloat {
         get { self[DefaultButtonCornerRadiusKey.self] }
         set { self[DefaultButtonCornerRadiusKey.self] = newValue }
@@ -47,11 +47,11 @@ extension EnvironmentValues {
 
 // MARK: - DefaultAppTypeface
 
-private struct DefaultAppTypefaceKey: EnvironmentKey {
-    static var defaultValue = UIFont.defaultAppTypeface
-}
-
 extension EnvironmentValues {
+    private struct DefaultAppTypefaceKey: EnvironmentKey {
+        static var defaultValue = UIFont.defaultAppTypeface
+    }
+
     // TODO: Implement the app function to pick the typeface from environment.
     var defaultAppTypeface: UIFont.Typeface {
         get { self[DefaultAppTypefaceKey.self] }
