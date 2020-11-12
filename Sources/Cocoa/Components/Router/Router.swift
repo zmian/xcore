19bc7398106a1274a65bb8cee13dcb7dcc6fd308
Zeeshan Mian <zmian@me.com>
2020-11-11 19:58:06 -0500

    Removed the compiler flag

--- Sources/Cocoa/Components/Router/Router.swift
+++ Sources/Cocoa/Components/Router/Router.swift
@@ -8,14 +8,14 @@ import UIKit
 
 public class Router {
     private weak var navigationController: UINavigationController?
-    private var routeHandlers: [String: Any] = [:]
+    private var routeHandlers: [ObjectIdentifier: Any] = [:]
 
     public init(navigationController: UINavigationController?) {
         self.navigationController = navigationController
     }
 
     public func register<T: RouteHandler>(_ handler: @autoclosure () -> T) -> T {
-        let key = name(of: T.self)
+        let key = ObjectIdentifier(T.self)
 
         guard let existingHandler = routeHandlers[key] as? T else {
             let handler = handler()
