32037da0c67e58914eea0d3765602b9c22c0c938
Zeeshan Mian <zmian@me.com>
2020-12-02 21:05:47 -0500

    Added Appliable conformance.

--- Sources/Swift/Extensions/Codable/Codable+Extensions.swift
+++ Sources/Swift/Extensions/Codable/Codable+Extensions.swift
@@ -6,12 +6,21 @@
 
 import Foundation
 
+// MARK: - Appliable
+
+extension JSONDecoder: Appliable { }
+extension JSONEncoder: Appliable { }
+extension PropertyListDecoder: Appliable { }
+extension PropertyListEncoder: Appliable { }
+
+// MARK: - KeyedDecodingContainer
+
 extension KeyedDecodingContainer {
     public func decode<T: Decodable>(_ key: Key) throws -> T {
         try decode(T.self, forKey: key)
     }
 
-    public func decodeIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) throws -> T? {
+    public func decodeIfPresent<T: Decodable>(_ key: Key) throws -> T? {
         try decodeIfPresent(T.self, forKey: key)
     }
 }
