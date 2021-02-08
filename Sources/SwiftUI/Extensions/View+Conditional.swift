ce18d034fcead593dc29a69e19550d87e5a88be9
Zeeshan Mian <zmian@me.com>
2021-02-07 22:51:01 -0500

    Removed Xcode 11.7 support.

--- Sources/SwiftUI/Extensions/View+Conditional.swift
+++ Sources/SwiftUI/Extensions/View+Conditional.swift
@@ -9,17 +9,15 @@ import SwiftUI
 // MARK: - Unwrap
 
 extension View {
+    @ViewBuilder
     public func unwrap<Value, Content>(
         _ value: Value?,
         _ content: (Self, Value) -> Content
     ) -> some View where Content: View {
-        Group {
-            // TODO: Xcode 12 switch to if let.
-            if value != nil {
-                content(self, value!)
-            } else {
-                self
-            }
+        if let value = value {
+            content(self, value)
+        } else {
+            self
         }
     }
 }
@@ -28,86 +26,80 @@ extension View {
 
 extension View {
     /// Applies modifier when given condition is satisfied.
+    @ViewBuilder
     func when<Content>(
         _ condition: @autoclosure () -> Bool,
         _ modifier: @autoclosure () -> Content
     ) -> some View where Content: ViewModifier {
-        Group {
-            if condition() {
-                self.modifier(modifier())
-            } else {
-                self
-            }
+        if condition() {
+            self.modifier(modifier())
+        } else {
+            self
         }
     }
 
     /// Applies modifier when given condition is satisfied.
+    @ViewBuilder
     func when<Content>(
         _ condition: @autoclosure () -> Bool,
         @ViewBuilder _ modifier: () -> Content
     ) -> some View where Content: ViewModifier {
-        Group {
-            if condition() {
-                self.modifier(modifier())
-            } else {
-                self
-            }
+        if condition() {
+            self.modifier(modifier())
+        } else {
+            self
         }
     }
 
     /// Add content when given condition is satisfied.
+    @ViewBuilder
     func when<Content>(
         _ condition: @autoclosure () -> Bool,
         _ content: @autoclosure () -> Content
     ) -> some View where Content: View {
-        Group {
-            if condition() {
-                content()
-            } else {
-                self
-            }
+        if condition() {
+            content()
+        } else {
+            self
         }
     }
 
     /// Add content when given condition is satisfied.
+    @ViewBuilder
     func when<Content>(
         _ condition: @autoclosure () -> Bool,
         @ViewBuilder _ content: () -> Content
     ) -> some View where Content: View {
-        Group {
-            if condition() {
-                content()
-            } else {
-                self
-            }
+        if condition() {
+            content()
+        } else {
+            self
         }
     }
 
     /// Add content when given condition is satisfied.
+    @ViewBuilder
     public func when<Content>(
         _ condition: Bool,
         @ViewBuilder _ content: (Self) -> Content
     ) -> some View where Content: View {
-        Group {
-            if condition {
-                content(self)
-            } else {
-                self
-            }
+        if condition {
+            content(self)
+        } else {
+            self
         }
     }
 
     /// Add content when given condition is satisfied.
+    @ViewBuilder
     public func when<Content>(
         _ condition: Binding<Bool>,
         @ViewBuilder _ content: (Self) -> Content
     ) -> some View where Content: View {
-        Group {
-            if condition.wrappedValue {
-                content(self)
-            } else {
-                self
-            }
+        if condition.wrappedValue {
+            content(self)
+        } else {
+            self
         }
     }
 }
