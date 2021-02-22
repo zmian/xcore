cb9e672ada7359aa51d00faaac1aef21ecee3b48
Zeeshan Mian <zmian@me.com>
2021-02-22 10:33:34 -0500

    Added userInfo key.

--- Sources/Swift/Components/FeatureFlag/FeatureFlag+Key.swift
+++ Sources/Swift/Components/FeatureFlag/FeatureFlag+Key.swift
@@ -14,11 +14,28 @@ public enum FeatureFlag { }
 // MARK: - Key
 
 extension FeatureFlag {
-    public struct Key: RawRepresentable, Equatable {
+    public struct Key: RawRepresentable, Equatable, UserInfoContainer {
         public let rawValue: String
 
+        /// Additional info which may be used to describe the key further.
+        public var userInfo: UserInfo
+
+        /// Initialize an instance of key.
+        ///
+        /// - Parameter rawValue: A unique string representing the key.
         public init(rawValue: String) {
             self.rawValue = rawValue
+            self.userInfo = [:]
+        }
+
+        /// Initialize an instance of key.
+        ///
+        /// - Parameters:
+        ///   - rawValue: A unique string representing the key.
+        ///   - userInfo: Additional info associated with the key.
+        public init(rawValue: String, userInfo: UserInfo) {
+            self.rawValue = rawValue
+            self.userInfo = userInfo
         }
     }
 }
