53a6d1008d92eb5bc74b694754890d036e4ef1d8
Zeeshan Mian <zmian@me.com>
2021-06-27 00:00:34 -0400

    Added EnvironmentReader

---
+++ Sources/SwiftUI/Components/EnvironmentReader.swift
@@ -0,0 +1,31 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+/// A view that reads an environment value of a key path.
+///
+/// ```
+/// EnvironmentReader(\.theme) { theme in
+///     Color(theme.backgroundColor)
+/// }
+/// ```
+public struct EnvironmentReader<Value, Content: View>: View {
+    @Environment private var value: Value
+    private let content: (Value) -> Content
+
+    public init(
+        _ keyPath: KeyPath<EnvironmentValues, Value>,
+        @ViewBuilder content: @escaping (Value) -> Content
+    ) {
+        self._value = .init(keyPath)
+        self.content = content
+    }
+
+    public var body: some View {
+        content(value)
+    }
+}
