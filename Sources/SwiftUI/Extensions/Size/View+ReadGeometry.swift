6a41b5d23fb1db06ec4bce9815cd7448147b2594
Zeeshan Mian <zmian@me.com>
2021-07-03 22:06:43 -0400

    Split roadsize into it's own file.

---
+++ Sources/SwiftUI/Extensions/Size/View+ReadGeometry.swift
@@ -0,0 +1,44 @@
+//
+// Xcore
+// Copyright © 2020 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+private struct GeometryPreferenceKey: PreferenceKey {
+    static var defaultValue: GeometryProxyWrapper?
+    static func reduce(value: inout Value, nextValue: () -> Value) { }
+}
+
+extension View {
+    /// Adds a modifier for this view that fires an action when view's geometry changes.
+    public func readGeometry(perform action: @escaping (GeometryProxy) -> Void) -> some View {
+        background(
+            GeometryReader { geometry in
+                Color.clear
+                    .preference(key: GeometryPreferenceKey.self, value: .init(base: geometry))
+            }
+        )
+        .onPreferenceChange(GeometryPreferenceKey.self) {
+            guard let proxy = $0?.base else {
+                return
+            }
+
+            action(proxy)
+        }
+    }
+}
+
+// MARK: - Wrapper
+
+private struct GeometryProxyWrapper: Equatable {
+    let base: GeometryProxy
+
+    static func == (lhs: GeometryProxyWrapper, rhs: GeometryProxyWrapper) -> Bool {
+        lhs.base.size == rhs.base.size &&
+        lhs.base.safeAreaInsets == rhs.base.safeAreaInsets &&
+        lhs.base.frame(in: .global) == rhs.base.frame(in: .global) &&
+        lhs.base.frame(in: .local) == rhs.base.frame(in: .local)
+    }
+}
---
+++ Sources/SwiftUI/Extensions/Size/View+ReadSize.swift
@@ -0,0 +1,34 @@
+//
+// Xcore
+// Copyright © 2020 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+private struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) { }
+}
+
+extension View {
+    /// Adds a modifier for this view that binds view size to specified binding.
+    public func readSize(_ size: Binding<CGSize>) -> some View {
+        readSize {
+            size.wrappedValue = $0
+        }
+    }
+
+    /// Adds a modifier for this view that fires an action when view's size changes.
+    ///
+    /// - SeeAlso: https://fivestars.blog/swiftui/swiftui-share-layout-information.html
+    public func readSize(perform action: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geometry in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometry.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: action)
+    }
+}
