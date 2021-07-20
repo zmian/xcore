c6f3dce2ca366e88bacc9f8ba8668e9026bb0787
Zeeshan Mian <zmian@me.com>
2021-07-20 15:50:33 -0400

    Added item based popup method.

--- Sources/Xcore/SwiftUI/Components/Popup/View+Popup.swift
+++ Sources/Xcore/SwiftUI/Components/Popup/View+Popup.swift
@@ -9,18 +9,134 @@ import SwiftUI
 // MARK: - View Extension
 
 extension View {
-    public func popup<Content: View>(
+    /// Presents a popup when a binding to a Boolean value that you provide is
+    /// `true`.
+    ///
+    /// Use this method when you want to present a popup view to the user when a
+    /// Boolean value you provide is true. The example below displays a modal view
+    /// of the mockup for a software license agreement when the user toggles the
+    /// `isShowingPopup` variable by clicking or tapping on the "Show License
+    /// Agreement" button:
+    ///
+    /// ```swift
+    /// struct ShowLicenseAgreement: View {
+    ///     @State private var isShowingPopup = false
+    ///
+    ///     var body: some View {
+    ///         Button {
+    ///             isShowingPopup.toggle()
+    ///         } label: {
+    ///             Text("Show License Agreement")
+    ///         }
+    ///         .popup(isPresented: $isShowingPopup) {
+    ///             VStack {
+    ///                 Text("License Agreement")
+    ///                 Text("Terms and conditions go here.")
+    ///                 Button("Dismiss") {
+    ///                     isShowingPopup.toggle()
+    ///                 }
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - isPresented: A binding to a Boolean value that determines whether to
+    ///     present the popup that you create in the modifier's `content` closure.
+    ///   - style: The style of the popup.
+    ///   - dismissMethods: An option set specifying the dismissal methods for the
+    ///     popup.
+    ///   - content: A closure returning the content of the popup.
+    ///   - onDismiss: The closure to execute when dismissing the popup.
+    public func popup<Content>(
         isPresented: Binding<Bool>,
         style: Popup.Style = .alert,
         dismissMethods: Popup.DismissMethods = [.tapOutside],
         @ViewBuilder content: @escaping () -> Content,
         onDismiss: (() -> Void)? = nil
-    ) -> some View {
+    ) -> some View where Content: View {
         modifier(PopupViewModifier(
             isPresented: isPresented,
             style: style,
             dismissMethods: dismissMethods,
             content: content,
+            onDismiss: onDismiss
+        ))
+    }
+
+    /// Presents a popup using the given item as a data source for the popup's
+    /// content.
+    ///
+    /// Use this method when you need to present a modal view with content from a
+    /// custom data source. The example below shows a custom data source
+    /// `InventoryItem` that the `content` closure uses to populate the display the
+    /// action popup shows to the user:
+    ///
+    /// ```swift
+    /// struct ShowPartDetail: View {
+    ///     @State var popupDetail: InventoryItem?
+    ///
+    ///     var body: some View {
+    ///         Button("Show Part Details") {
+    ///             popupDetail = InventoryItem(
+    ///                 id: "0123456789",
+    ///                 partNumber: "Z-1234A",
+    ///                 quantity: 100,
+    ///                 name: "Widget"
+    ///             )
+    ///         }
+    ///         .popup(item: $popupDetail) { detail in
+    ///             VStack(alignment: .leading, spacing: 20) {
+    ///                 Text("Part Number: \(detail.partNumber)")
+    ///                 Text("Name: \(detail.name)")
+    ///                 Text("Quantity On-Hand: \(detail.quantity)")
+    ///             }
+    ///             .onTapGesture {
+    ///                 popupDetail = nil
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// struct InventoryItem: Identifiable {
+    ///     var id: String
+    ///     let partNumber: String
+    ///     let quantity: Int
+    ///     let name: String
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - item: A binding to an optional source of truth for the popup. When
+    ///    `item` is non-`nil`, the system passes the item's content to the
+    ///     modifier's closure. You display this content in a popup that you create
+    ///     that the system displays to the user. If `item` changes, the system
+    ///     dismisses the popup and replaces it with a new one using the same
+    ///     process.
+    ///   - style: The style of the popup.
+    ///   - dismissMethods: An option set specifying the dismissal methods for the
+    ///     popup.
+    ///   - content: A closure returning the content of the popup.
+    ///   - onDismiss: The closure to execute when dismissing the popup.
+    public func popup<Item, Content>(
+        item: Binding<Item?>,
+        style: Popup.Style = .alert,
+        dismissMethods: Popup.DismissMethods = [.tapOutside],
+        @ViewBuilder content: @escaping (Item) -> Content,
+        onDismiss: (() -> Void)? = nil
+    ) -> some View where Item: Identifiable, Content: View {
+        modifier(PopupViewModifier(
+            isPresented: .init {
+                item.wrappedValue != nil
+            } set: { newValue in
+                item.wrappedValue = nil
+            },
+            style: style,
+            dismissMethods: dismissMethods,
+            content: {
+                content(item.wrappedValue!)
+            },
             onDismiss: onDismiss
         ))
     }
