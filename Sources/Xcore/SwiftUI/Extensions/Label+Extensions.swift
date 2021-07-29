81458b8245715f46145ee36d76efa51ab7933f5b
Zeeshan Mian <zmian@me.com>
2021-07-29 15:37:59 -0400

    Added image extension to label

--- Sources/Xcore/SwiftUI/Extensions/Label+Extensions.swift
+++ Sources/Xcore/SwiftUI/Extensions/Label+Extensions.swift
@@ -6,6 +6,8 @@
 
 import SwiftUI
 
+// MARK: - SystemAssetIdentifier
+
 extension Label where Title == Text, Icon == Image {
     /// Creates a label with a system icon image and a title generated from a
     /// localized string.
@@ -25,5 +27,51 @@ extension Label where Title == Text, Icon == Image {
     ///    - systemImage: The name of the image resource to lookup.
     public init<S>(_ title: S, systemImage: SystemAssetIdentifier) where S: StringProtocol {
         self.init(title, systemImage: systemImage.rawValue)
+    }
+}
+
+// MARK: - ImageAssetIdentifier
+
+extension Label where Title == Text, Icon == Image {
+    /// Creates a label with an icon image and a title generated from a localized
+    /// string.
+    ///
+    /// - Parameters:
+    ///    - titleKey: A title generated from a localized string.
+    ///    - image: The name of the image resource to lookup.
+    public init(_ titleKey: LocalizedStringKey, image: ImageAssetIdentifier) {
+        self.init(titleKey, image: Image(assetIdentifier: image))
+    }
+
+    /// Creates a label with an icon image and a title generated from a string.
+    ///
+    /// - Parameters:
+    ///    - title: A string to used as the label's title.
+    ///    - image: The name of the image resource to lookup.
+    public init<S>(_ title: S, image: ImageAssetIdentifier) where S: StringProtocol {
+        self.init(title, image: Image(assetIdentifier: image))
+    }
+}
+
+// MARK: - Image
+
+extension Label where Title == Text, Icon == Image {
+    /// Creates a label with an icon image and a title generated from a localized
+    /// string.
+    ///
+    /// - Parameters:
+    ///    - titleKey: A title generated from a localized string.
+    ///    - image: The image.
+    public init(_ titleKey: LocalizedStringKey, image: Image) {
+        self.init { Text(titleKey) } icon: { image }
+    }
+
+    /// Creates a label with an icon image and a title generated from a string.
+    ///
+    /// - Parameters:
+    ///    - title: A string to used as the label's title.
+    ///    - image: The image.
+    public init<S>(_ title: S, image: Image) where S: StringProtocol {
+        self.init { Text(title) } icon: { image }
     }
 }
