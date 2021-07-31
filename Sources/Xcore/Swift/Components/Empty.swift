8f7284f6fc2bb74cb11fd75b6be8d2d24c71fb55
Zeeshan Mian <zmian@me.com>
2021-07-31 16:51:17 -0400

    Added Empty type

---
+++ Sources/Xcore/Swift/Components/Empty.swift
@@ -0,0 +1,38 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import Foundation
+import SwiftUI
+
+/// A structure representing empty value.
+@frozen
+public struct Empty {
+    public init() {}
+}
+
+extension Empty: Sendable {}
+extension Empty: Equatable, Hashable {}
+extension Empty: Codable {}
+
+extension Empty: Identifiable {
+    public var id: String { "" }
+}
+
+extension Empty: Comparable {
+    public static func <(lhs: Self, rhs: Self) -> Bool {
+        false
+    }
+
+    public static func > (lhs: Self, rhs: Self) -> Bool {
+        false
+    }
+}
+
+extension Empty: View {
+    public var body: some View {
+        EmptyView()
+    }
+}
