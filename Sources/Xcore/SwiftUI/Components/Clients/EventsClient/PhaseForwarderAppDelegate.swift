adf7208a2c2f82aaf8821989e684081516adcda1
Zeeshan Mian <zmian@me.com>
2021-08-14 18:04:36 -0400

    Added PhaseForwarderAppDelegate

---
+++ Sources/Xcore/SwiftUI/Components/Clients/EventsClient/PhaseForwarderAppDelegate.swift
@@ -0,0 +1,136 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import UIKit
+
+/// A class to forward app delegate phase events to the given send method.
+///
+/// **Usage**
+///
+/// ```swift
+/// // 1. Send events to `AppDelegatePhaseClient`
+///
+/// final class AppDelegate: PhaseForwarderAppDelegate {
+///     @Dependency(\.appDelegatePhase) var appPhase
+///
+///     override init() {
+///         super.init()
+///         send = { [weak self] in
+///             self?.appPhase.send($0)
+///         }
+///     }
+/// }
+///
+/// // 2. Receive events from `AppDelegatePhaseClient`
+///
+/// struct SegmentAnalyticsProvider: AnalyticsProvider {
+///     @Dependency(\.appDelegatePhase) private var appPhase
+///     private var cancellable: AnyCancellable?
+///
+///     ...
+///
+///     private mutating func addListener() {
+///         cancellable = appPhase.receive.sink { phase in
+///             let segment = Segment.Analytics.shared()
+///
+///             switch phase {
+///                 case let .remoteNotificationsRegistered(.success(token)):
+///                     segment.registeredForRemoteNotifications(withDeviceToken: token)
+///                 case let .remoteNotificationsRegistered(.failure(error)):
+///                     segment.failedToRegisterForRemoteNotificationsWithError(error)
+///                 case let .remoteNotificationReceived(userInfo):
+///                     segment.receivedRemoteNotification(userInfo)
+///                 case let .continueUserActivity(activity, _):
+///                     segment.continue(activity)
+///                 case let .openUrl(url, options):
+///                     segment.open(url, options: options)
+///                 default:
+///                     break
+///             }
+///         }
+///     }
+/// }
+/// ```
+open class PhaseForwarderAppDelegate: UIResponder, UIApplicationDelegate {
+    public var send: (AppDelegatePhase) -> Void = { _ in }
+
+    open func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        send(.finishedLaunching)
+        return true
+    }
+
+    // MARK: - Responding to App Life-Cycle Events
+
+    open func applicationDidBecomeActive(_ application: UIApplication) {
+        send(.didBecomeActive)
+    }
+
+    open func applicationWillResignActive(_ application: UIApplication) {
+        send(.willResignActive)
+    }
+
+    open func applicationDidEnterBackground(_ application: UIApplication) {
+        send(.didEnterBackground)
+    }
+
+    open func applicationWillEnterForeground(_ application: UIApplication) {
+        send(.willEnterForeground)
+    }
+
+    open func applicationWillTerminate(_ application: UIApplication) {
+        send(.willTerminate)
+    }
+
+    // MARK: - Handling Remote Notification Registration
+
+    open func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        send(.remoteNotificationsRegistered(.success(deviceToken)))
+    }
+
+    open func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        send(.remoteNotificationsRegistered(.failure(error as NSError)))
+    }
+
+    open func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        send(.remoteNotificationReceived(userInfo: userInfo))
+        completionHandler(.noData)
+    }
+
+    // MARK: - Opening a URL-Specified Resource
+
+    open func application(
+        _ application: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        send(.openUrl(url, options: options))
+        return true
+    }
+
+    // MARK: - Continuing User Activity and Handling Quick Actions
+
+    open func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        send(.continueUserActivity(userActivity, handler: restorationHandler))
+        return true
+    }
+}
