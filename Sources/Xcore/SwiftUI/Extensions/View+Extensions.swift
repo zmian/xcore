06a8759daef84a9a55ec0c5bb1c66f69a14407c7
Zeeshan Mian <zmian@me.com>
2021-09-12 14:43:44 -0400

    Added Spacer extension

--- Sources/Xcore/SwiftUI/Extensions/View+Extensions.swift
+++ Sources/Xcore/SwiftUI/Extensions/View+Extensions.swift
@@ -23,10 +23,12 @@ extension View {
 // MARK: - Colors
 
 extension View {
-    /// Sets the color that the view uses for foreground elements.
-    @_disfavoredOverload
-    public func foregroundColor(_ color: UIColor) -> some View {
-        foregroundColor(Color(color))
+    /// Sets the background color behind this view.
+    public func backgroundColor(_ color: Color, ignoresSafeAreaEdges edges: Edge.Set = .all) -> some View {
+        background(
+            color
+                .ignoresSafeArea(edges: edges)
+        )
     }
 
     /// Sets the background color behind this view.
@@ -38,12 +40,10 @@ extension View {
         )
     }
 
-    /// Sets the background color behind this view.
-    public func backgroundColor(_ color: Color, ignoresSafeAreaEdges edges: Edge.Set = .all) -> some View {
-        background(
-            color
-                .ignoresSafeArea(edges: edges)
-        )
+    /// Sets the color that the view uses for foreground elements.
+    @_disfavoredOverload
+    public func foregroundColor(_ color: UIColor) -> some View {
+        foregroundColor(Color(color))
     }
 }
 
@@ -87,4 +87,27 @@ extension View {
             self
         }
     }
+}
+
+// MARK: - Accessibility
+
+extension View {
+    /// Adds a label to the view that describes its contents.
+    ///
+    /// Use this method to provide an accessibility label for a view that doesn’t
+    /// display text, like an icon. For example, you could use this method to label
+    /// a button that plays music with the text “Play”. Don’t include text in the
+    /// label that repeats information that users already have. For example, don’t
+    /// use the label “Play button” because a button already has a trait that
+    /// identifies it as a button.
+    public func accessibilityLabel(_ label: String?...) -> some View {
+        accessibilityLabel(Text(label.joined(separator: ", ")))
+    }
+}
+
+// MARK: - Spacer
+
+public func Spacer(height: CGFloat) -> some View {
+    Spacer()
+        .frame(height: height)
 }
