0870bd8d3572411b2c4ab4bac11cd351b31cd1d2
Zeeshan Mian <zmian@me.com>
2021-11-05 22:34:25 -0400

    Added support for tint color to the `overlayLoader`

--- Sources/Xcore/SwiftUI/Extensions/View+Loader.swift
+++ Sources/Xcore/SwiftUI/Extensions/View+Loader.swift
@@ -8,14 +8,33 @@ import SwiftUI
 
 extension View {
     /// Layers a loader in front of this view when the given flag is `true`.
-    public func overlayLoader(_ show: Bool, alignment: Alignment = .center) -> some View {
-        overlay(ProgressView().hidden(!show), alignment: alignment)
+    public func overlayLoader(
+        _ show: Bool,
+        tint: Color? = nil,
+        alignment: Alignment = .center
+    ) -> some View {
+        overlay(
+            ProgressView()
+                .unwrap(tint) { content, tint in
+                    if #available(iOS 15.0, *) {
+                        content.tint(tint)
+                    } else {
+                        content.progressViewStyle(CircularProgressViewStyle(tint: tint))
+                    }
+                }
+                .hidden(!show),
+            alignment: alignment
+        )
     }
 
     /// Sets the transparency of this view to `0` when the given flag is `true` and
     /// layers a loader in front of this view.
-    public func maskWithLoader(when value: Bool, alignment: Alignment = .center) -> some View {
+    public func maskWithLoader(
+        when value: Bool,
+        tint: Color? = nil,
+        alignment: Alignment = .center
+    ) -> some View {
         opacity(value ? 0 : 1)
-            .overlayLoader(value, alignment: alignment)
+            .overlayLoader(value, tint: tint, alignment: alignment)
     }
 }
