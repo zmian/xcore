a207207576b7acd135d8e84f3b00bb9005bff3ea
Zeeshan Mian <zmian@me.com>
2021-11-05 22:07:44 -0400

    Added loader extensions

---
+++ Sources/Xcore/SwiftUI/Extensions/View+Loader.swift
@@ -0,0 +1,21 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension View {
+    /// Layers a loader in front of this view when the given flag is `true`.
+    public func overlayLoader(_ show: Bool, alignment: Alignment = .center) -> some View {
+        overlay(ProgressView().hidden(!show), alignment: alignment)
+    }
+
+    /// Sets the transparency of this view to `0` when the given flag is `true` and
+    /// layers a loader in front of this view.
+    public func maskWithLoader(when value: Bool, alignment: Alignment = .center) -> some View {
+        opacity(value ? 0 : 1)
+            .overlayLoader(value, alignment: alignment)
+    }
+}
