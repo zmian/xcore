34d6eb93a4fc3d385cf399cdfeee52c481221b68
Zeeshan Mian <zmian@me.com>
2021-11-12 17:43:13 -0500

    Added popup sheets and clean up the implementation

---
+++ Sources/Xcore/SwiftUI/Components/Popup/Content/PopupSheet.swift
@@ -0,0 +1,54 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+/// A representation of a sheet presentation.
+public struct PopupSheet<Content>: View where Content: View {
+    @Environment(\.theme) private var theme
+    @Environment(\.popupCornerRadius) private var cornerRadius
+    @Environment(\.defaultMinListRowHeight) private var rowHeight
+    @State private var safeAreaInsetsBottom: CGFloat = 0
+    private let content: () -> Content
+
+    public init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+
+    public var body: some View {
+        VStack(spacing: 0) {
+            content()
+                .frame(maxWidth: .infinity, minHeight: rowHeight)
+            Spacer(height: safeAreaInsetsBottom)
+        }
+        .backgroundColor(theme.backgroundColor)
+        .clipShape(RoundedRectangleCorner(radius: cornerRadius, corners: .top))
+        .fixedSize(horizontal: false, vertical: true)
+        .readGeometry {
+            safeAreaInsetsBottom = $0.safeAreaInsets.bottom
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct PopupSheet_Previews: PreviewProvider {
+    private static let L = Samples.Strings.deleteMessageAlert
+
+    static var previews: some View {
+        PopupSheet {
+            List {
+                Text("Swift")
+                Text("Swift")
+                Text("Swift")
+            }
+            .frame(height: 250)
+        }
+        .frame(max: .infinity, alignment: .bottom)
+        .backgroundColor(.secondary)
+        .ignoresSafeArea()
+    }
+}
--- Sources/Xcore/SwiftUI/Components/Popup/Popup+Environment.swift
+++ Sources/Xcore/SwiftUI/Components/Popup/Popup+Environment.swift
@@ -6,39 +6,58 @@
 
 import SwiftUI
 
-// MARK: - Environment Support
+// MARK: - Keys
 
 extension EnvironmentValues {
-    private struct PopupAlertAttributesKey: EnvironmentKey {
-        static var defaultValue: Popup.AlertAttributes = .init()
+    private struct PopupCornerRadiusKey: EnvironmentKey {
+        static var defaultValue: CGFloat = 16
     }
 
-    var popupAlertAttributes: Popup.AlertAttributes {
-        get { self[PopupAlertAttributesKey.self] }
-        set { self[PopupAlertAttributesKey.self] = newValue }
+    private struct PopupPreferredWidthKey: EnvironmentKey {
+        static var defaultValue: CGFloat = AppConstants.popupPreferredWidth
+    }
+
+    private struct PopupTextAlignmentKey: EnvironmentKey {
+        static var defaultValue: TextAlignment = .center
+    }
+}
+
+// MARK: - Values
+
+extension EnvironmentValues {
+    var popupCornerRadius: CGFloat {
+        get { self[PopupCornerRadiusKey.self] }
+        set { self[PopupCornerRadiusKey.self] = newValue }
+    }
+
+    var popupPreferredWidth: CGFloat {
+        get { self[PopupPreferredWidthKey.self] }
+        set { self[PopupPreferredWidthKey.self] = newValue }
+    }
+
+    var popupTextAlignment: TextAlignment {
+        get { self[PopupTextAlignmentKey.self] }
+        set { self[PopupTextAlignmentKey.self] = newValue }
     }
 }
 
 // MARK: - View Helpers
 
 extension View {
-    public func popupAlert(
-        cornerRadius: CGFloat = 16,
-        width: CGFloat = 300,
-        textAlignment: TextAlignment = .center
-    ) -> some View {
-        transformEnvironment(\.popupAlertAttributes) {
-            $0.cornerRadius = cornerRadius
-            $0.width = width
-            $0.textAlignment = textAlignment
-        }
+    /// Clips popups within the environment to its bounding frame, with the
+    /// specified corner radius.
+    public func popupCornerRadius(_ cornerRadius: CGFloat) -> some View {
+        environment(\.popupCornerRadius, cornerRadius)
     }
-}
 
-extension Popup {
-    struct AlertAttributes {
-        var cornerRadius: CGFloat = 16
-        var width: CGFloat = 300
-        var textAlignment: TextAlignment = .center
+    /// Sets popups width within the environment to the specified value.
+    public func popupPreferredWidth(_ width: CGFloat) -> some View {
+        environment(\.popupPreferredWidth, width)
+    }
+
+    /// Sets the alignment of popups text within the environment to the specified
+    /// value.
+    public func popupTextAlignment(_ textAlignment: TextAlignment) -> some View {
+        environment(\.popupTextAlignment, textAlignment)
     }
 }
