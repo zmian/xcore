a0169f95750632ed90918dd2f9839c517d54e720
Zeeshan Mian <zmian@me.com>
2021-11-26 13:01:41 -0500

    Added stub and failing pond variants.

---
+++ Sources/Xcore/SwiftUI/Components/Pond/Pond+Failing.swift
@@ -0,0 +1,38 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+#if DEBUG
+import Foundation
+
+public struct FailingPond: Pond {
+    public init() {}
+
+    public func get<T>(_ type: T.Type, _ key: Key) -> T? {
+        internal_XCTFail("\(Self.self).get is unimplemented")
+        return nil
+    }
+
+    public func set<T>(_ key: Key, value: T?) {
+        internal_XCTFail("\(Self.self).set is unimplemented")
+    }
+
+    public func remove(_ key: Key) {
+        internal_XCTFail("\(Self.self).remove is unimplemented")
+    }
+
+    public func contains(_ key: Key) -> Bool {
+        internal_XCTFail("\(Self.self).contains is unimplemented")
+        return false
+    }
+}
+
+// MARK: - Dot Syntax Support
+
+extension Pond where Self == FailingPond {
+    /// Returns failing variant of `Pond`.
+    public static var failing: Self { .init() }
+}
+#endif
