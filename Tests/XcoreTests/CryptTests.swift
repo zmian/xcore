148786a4c336eaf565cebb768fe16910feaf48b7
Zeeshan Mian <zmian@me.com>
2021-11-25 19:25:21 -0500

    Enabled support for string based code

--- Tests/XcoreTests/CryptTests.swift
+++ Tests/XcoreTests/CryptTests.swift
@@ -14,18 +14,16 @@ final class CryptTests: TestCase {
 
         let obfuscated = Crypt.obfuscate(message, secret: secret)
         let deobfuscate = try Crypt.deobfuscate(obfuscated, secret: secret)
-        let obfuscatedString = try XCTUnwrap(String(data: Data(obfuscated), encoding: .utf8))
 
-        XCTAssertNotEqual(message, obfuscatedString)
+        XCTAssertNotEqual(Data(message.utf8).sha256().bytes, obfuscated)
         XCTAssertEqual(message, deobfuscate)
 
         // Incorrect Secret
-        let incorrectSecret = Crypt.generateRandomPassword() + "1"
-        let incorrectDeobfuscate = try Crypt.deobfuscate(obfuscated, secret: incorrectSecret)
-        XCTAssertNotEqual(message, incorrectDeobfuscate)
+        let incorrectSecret = Crypt.generateRandomPassword()
+        XCTAssertThrowsError(try Crypt.deobfuscate(obfuscated, secret: incorrectSecret))
     }
 
-    func testEncryptDecrypt() throws {
+    func testEncryptDecrypt_SecretData() throws {
         let secret = Crypt.generateSecureRandom()
         let message = try XCTUnwrap("Hello World".data(using: .utf8))
 
@@ -39,6 +37,23 @@ final class CryptTests: TestCase {
 
         // Incorrect Secret
         let incorrectSecret = Crypt.generateSecureRandom()
+        XCTAssertThrowsError(try Crypt.decrypt(encryptedMessage, secret: incorrectSecret))
+    }
+
+    func testEncryptDecrypt_SecretString() throws {
+        let secret = Crypt.generateRandomPassword()
+        let message = try XCTUnwrap("Hello World".data(using: .utf8))
+
+        let encryptedMessage = try Crypt.encrypt(message, secret: secret)
+        let decryptedMessage = try Crypt.decrypt(encryptedMessage, secret: secret)
+        let decryptedMessageString = try XCTUnwrap(String(data: decryptedMessage, encoding: .utf8))
+
+        XCTAssertEqual("Hello World", decryptedMessageString)
+        XCTAssertEqual(message, decryptedMessage)
+        XCTAssertNotEqual(message, encryptedMessage)
+
+        // Incorrect Secret
+        let incorrectSecret = Crypt.generateRandomPassword()
         XCTAssertThrowsError(try Crypt.decrypt(encryptedMessage, secret: incorrectSecret))
     }
 }
