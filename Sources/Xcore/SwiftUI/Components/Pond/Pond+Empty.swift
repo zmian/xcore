ad4ec833d66934af3284010804c42c2d1b4e58cb
Zeeshan Mian <zmian@me.com>
2021-11-26 01:28:07 -0500

    Created an empty pond

---
+++ Sources/Xcore/SwiftUI/Components/Pond/Pond+Empty.swift
@@ -0,0 +1,30 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import Foundation
+
+public struct EmptyPond: Pond {
+    public init() {}
+
+    public func get<T>(_ type: T.Type, _ key: Key) -> T? {
+        nil
+    }
+
+    public func set<T>(_ key: Key, value: T?) {}
+
+    public func remove(_ key: Key) {}
+
+    public func contains(_ key: Key) -> Bool {
+        false
+    }
+}
+
+// MARK: - Dot Syntax Support
+
+extension Pond where Self == EmptyPond {
+    /// Returns empty variant of `Pond`.
+    public static var empty: Self { .init() }
+}
