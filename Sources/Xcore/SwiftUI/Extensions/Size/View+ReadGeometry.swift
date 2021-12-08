56e474825775e72cb407601f080aef594a360511
Zeeshan Mian <zmian@me.com>
2021-12-08 01:20:35 -0500

    Added back the `readGeometryChange`

--- Sources/Xcore/SwiftUI/Extensions/Size/View+ReadGeometry.swift
+++ Sources/Xcore/SwiftUI/Extensions/Size/View+ReadGeometry.swift
@@ -7,8 +7,15 @@
 import SwiftUI
 
 extension View {
-    /// Adds a modifier for this view that fires an action when view's geometry
-    /// changes.
+    public func readOffsetY(_ offset: Binding<CGFloat>) -> some View {
+        readGeometry {
+            offset.wrappedValue = $0.frame(in: .global).minY
+        }
+    }
+}
+
+extension View {
+    /// Adds a modifier for this view that fires an action with geometry proxy.
     public func readGeometry(perform action: @escaping (GeometryProxy) -> Void) -> some View {
         background(
             GeometryReader { geometry -> Color in
@@ -17,12 +24,41 @@ extension View {
             }
         )
     }
+
+    /// Adds a modifier for this view that fires an action when view's geometry changes.
+    public func readGeometryChange(perform action: @escaping (GeometryProxy) -> Void) -> some View {
+        background(
+            GeometryReader { geometry in
+                Color.clear
+                    .preference(key: GeometryPreferenceKey.self, value: .init(base: geometry))
+            }
+        )
+        .onPreferenceChange(GeometryPreferenceKey.self) {
+            guard let proxy = $0?.base else {
+                return
+            }
+
+            action(proxy)
+        }
+    }
 }
 
-extension View {
-    public func readOffsetY(_ offset: Binding<CGFloat>) -> some View {
-        readGeometry {
-            offset.wrappedValue = $0.frame(in: .global).minY
-        }
+// MARK: - PreferenceKey
+
+private struct GeometryPreferenceKey: PreferenceKey {
+    static var defaultValue: GeometryProxyWrapper?
+    static func reduce(value: inout Value, nextValue: () -> Value) {}
+}
+
+// MARK: - Wrapper
+
+private struct GeometryProxyWrapper: Equatable {
+    let base: GeometryProxy
+
+    static func ==(lhs: GeometryProxyWrapper, rhs: GeometryProxyWrapper) -> Bool {
+        lhs.base.size == rhs.base.size &&
+            lhs.base.safeAreaInsets == rhs.base.safeAreaInsets &&
+            lhs.base.frame(in: .global) == rhs.base.frame(in: .global) &&
+            lhs.base.frame(in: .local) == rhs.base.frame(in: .local)
     }
 }
