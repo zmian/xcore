bce73d6bf80a92a4449ad05f14cb2300cbdf4fc0
Zeeshan Mian <zmian@me.com>
2021-12-07 20:56:58 -0500

    Added readOffset

--- Sources/Xcore/SwiftUI/Extensions/Size/View+ReadGeometry.swift
+++ Sources/Xcore/SwiftUI/Extensions/Size/View+ReadGeometry.swift
@@ -6,39 +6,23 @@
 
 import SwiftUI
 
-private struct GeometryPreferenceKey: PreferenceKey {
-    static var defaultValue: GeometryProxyWrapper?
-    static func reduce(value: inout Value, nextValue: () -> Value) {}
-}
-
 extension View {
-    /// Adds a modifier for this view that fires an action when view's geometry changes.
+    /// Adds a modifier for this view that fires an action when view's geometry
+    /// changes.
     public func readGeometry(perform action: @escaping (GeometryProxy) -> Void) -> some View {
         background(
-            GeometryReader { geometry in
-                Color.clear
-                    .preference(key: GeometryPreferenceKey.self, value: .init(base: geometry))
+            GeometryReader { geometry -> Color in
+                action(geometry)
+                return Color.clear
             }
         )
-        .onPreferenceChange(GeometryPreferenceKey.self) {
-            guard let proxy = $0?.base else {
-                return
-            }
-
-            action(proxy)
-        }
     }
 }
 
-// MARK: - Wrapper
-
-private struct GeometryProxyWrapper: Equatable {
-    let base: GeometryProxy
-
-    static func ==(lhs: GeometryProxyWrapper, rhs: GeometryProxyWrapper) -> Bool {
-        lhs.base.size == rhs.base.size &&
-            lhs.base.safeAreaInsets == rhs.base.safeAreaInsets &&
-            lhs.base.frame(in: .global) == rhs.base.frame(in: .global) &&
-            lhs.base.frame(in: .local) == rhs.base.frame(in: .local)
+extension View {
+    public func readOffsetY(_ offset: Binding<CGFloat>) -> some View {
+        readGeometry {
+            offset.wrappedValue = $0.frame(in: .global).minY
+        }
     }
 }
