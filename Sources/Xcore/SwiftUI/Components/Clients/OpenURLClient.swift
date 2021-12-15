f3202e3b4c723456c88a22f21b5482a6d0727b2f
Zeeshan Mian <zmian@me.com>
2021-12-14 21:45:24 -0500

    OpenURLClient now presents in full screen HUD.

--- Sources/Xcore/SwiftUI/Components/Clients/OpenURLClient.swift
+++ Sources/Xcore/SwiftUI/Components/Clients/OpenURLClient.swift
@@ -63,28 +63,6 @@ public struct OpenURLClient: Hashable, Identifiable {
 // MARK: - Variants
 
 extension OpenURLClient {
-    /// Returns system variant of `OpenURLClient`.
-    public static var system: Self {
-        .init { adaptiveUrl in
-            guard let app = UIApplication.sharedOrNil else {
-                return
-            }
-
-            let url = adaptiveUrl.url
-
-            // Attempt to open standard urls using in-app Safari.
-            if [.http, .https].contains(url.schemeType), let nvc = app.topNavigationController {
-                let vc = SFSafariViewController(url: url).apply {
-                    $0.preferredControlTintColor = Theme.default.tintColor
-                }
-                // Present shows the Safari VC correctly in SwiftUI.
-                nvc.present(vc, animated: true)
-            } else if app.canOpenURL(url) {
-                app.appExtensionSafeOpen(url)
-            }
-        }
-    }
-
     /// Returns noop variant of `OpenURLClient`.
     public static var noop: Self {
         .init { _ in }
@@ -98,6 +76,54 @@ extension OpenURLClient {
         }
     }
     #endif
+
+    /// Returns system variant of `OpenURLClient`.
+    public static var system: Self {
+        .init { adaptiveUrl in
+            guard let app = UIApplication.sharedOrNil else {
+                return
+            }
+
+            let url = adaptiveUrl.url
+
+            // Attempt to open standard urls using in-app Safari.
+            if [.http, .https].contains(url.schemeType) {
+                let vc = InAppSafariViewController(url: url).apply {
+                    $0.preferredControlTintColor = Theme.default.tintColor
+                }
+
+                // Present shows the Safari VC correctly in SwiftUI.
+                return vc.show()
+            } else if app.canOpenURL(url) {
+                app.appExtensionSafeOpen(url)
+            } else {
+                #if DEBUG
+                Console.warn("Unable to open the url: \(url)")
+                #endif
+            }
+        }
+    }
+
+    private final class InAppSafariViewController: SFSafariViewController {
+        private var hud = HUD().apply {
+            $0.backgroundColor = .clear
+            $0.passthroughNonContentTouches = true
+            $0.windowLabel = "OpenURL Window"
+
+            $0.adjustWindowAttributes {
+                $0.makeKey()
+            }
+        }
+
+        override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            hud.hide(animated: false)
+        }
+
+        func show() {
+            hud.present(self, animated: true)
+        }
+    }
 }
 
 // MARK: - Dependency
