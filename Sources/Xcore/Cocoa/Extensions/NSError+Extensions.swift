1ce7133cb965104b4f6f9bc9922fc6aa00c1bcd7
Zeeshan Mian <zmian@me.com>
2022-01-10 00:52:22 -0500

    Added `appendingUserInfo`

---
+++ Sources/Xcore/Cocoa/Extensions/NSError+Extensions.swift
@@ -0,0 +1,22 @@
+//
+// Xcore
+// Copyright © 2022 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import Foundation
+
+extension NSError {
+    /// Appends the given dictionary to the existing `userInfo` dictionary replacing
+    /// any existing values with newer ones.
+    public func appendingUserInfo(_ userInfo: [String: Any]) -> NSError {
+        var newUserInfo = self.userInfo
+        newUserInfo.merge(userInfo, strategy: .replaceExisting)
+
+        return .init(
+            domain: domain,
+            code: code,
+            userInfo: newUserInfo
+        )
+    }
+}
