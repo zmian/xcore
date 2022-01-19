ea5fc9c4bf563a3aa514e61a179d10fc098fcaa8
Zeeshan Mian <zmian@me.com>
2022-01-19 01:16:44 -0500

    Sorted the types

--- Sources/Xcore/SwiftUI/Components/Device/Biometrics.swift
+++ Sources/Xcore/SwiftUI/Components/Device/Biometrics.swift
@@ -143,60 +143,64 @@ extension Device {
 
 extension LAError: CustomStringConvertible {
     public var description: String {
-        switch self {
-            case LAError.authenticationFailed:
-                return "authenticationFailed"
-            case LAError.userCancel:
-                return "userCancel"
-            case LAError.userFallback:
-                return "userFallback"
-            case LAError.systemCancel:
-                return "systemCancel"
-            case LAError.passcodeNotSet:
-                return "passcodeNotSet"
-            case LAError.appCancel:
+        switch code {
+            case .appCancel:
                 return "appCancel"
-            case LAError.invalidContext:
-                return "invalidContext"
-            case LAError.biometryNotAvailable:
-                return "biometryNotAvailable"
-            case LAError.biometryNotEnrolled:
-                return "biometryNotEnrolled"
-            case LAError.biometryLockout:
+            case .userCancel:
+                return "userCancel"
+            case .systemCancel:
+                return "systemCancel"
+            case .biometryLockout:
                 return "biometryLockout"
-            case LAError.notInteractive:
+            case .biometryNotAvailable:
+                return "biometryNotAvailable"
+            case .biometryNotEnrolled:
+                return "biometryNotEnrolled"
+            case .authenticationFailed:
+                return "authenticationFailed"
+            case .invalidContext:
+                return "invalidContext"
+            case .notInteractive:
                 return "notInteractive"
+            case .passcodeNotSet:
+                return "passcodeNotSet"
+            case .userFallback:
+                return "userFallback"
+            case .watchNotAvailable:
+                return "watchNotAvailable"
             default:
-                return "unknown"
+                return "code_\(code.rawValue)"
         }
     }
 
     public var localizedDescription: String {
-        switch self {
-            case LAError.authenticationFailed:
-                return "Authentication was not successful because user failed to provide valid credentials."
-            case LAError.userCancel:
-                return "Authentication was canceled by user (e.g. tapped Cancel button)."
-            case LAError.userFallback:
-                return "Authentication was canceled because the user tapped the fallback button (Enter Password)."
-            case LAError.systemCancel:
-                return "Authentication was canceled by system (e.g. another application went to foreground)."
-            case LAError.passcodeNotSet:
-                return "Authentication could not start because passcode is not set on the device."
-            case LAError.appCancel:
+        switch code {
+            case .appCancel:
                 return "Authentication was canceled by application (e.g. invalidate was called while authentication was in progress)."
-            case LAError.invalidContext:
-                return "LAContext passed to this call has been previously invalidated."
-            case LAError.biometryNotAvailable:
-                return "Authentication could not start because biometry is not available on the device."
-            case LAError.biometryNotEnrolled:
-                return "Authentication could not start because biometry has no enrolled identities."
-            case LAError.biometryLockout:
+            case .userCancel:
+                return "Authentication was canceled by user (e.g. tapped Cancel button)."
+            case .systemCancel:
+                return "Authentication was canceled by system (e.g. another application went to foreground)."
+            case .biometryLockout:
                 return "Authentication was not successful because there were too many failed biometry attempts and biometry is now locked. Passcode is now required to unlock biometry."
-            case LAError.notInteractive:
+            case .biometryNotAvailable:
+                return "Authentication could not start because biometry is not available on the device."
+            case .biometryNotEnrolled:
+                return "Authentication could not start because biometry has no enrolled identities."
+            case .authenticationFailed:
+                return "Authentication was not successful because user failed to provide valid credentials."
+            case .invalidContext:
+                return "LAContext passed to this call has been previously invalidated."
+            case .notInteractive:
                 return "Authentication failed because it would require showing UI which has been forbidden by using \"interactionNotAllowed\" property."
+            case .passcodeNotSet:
+                return "Authentication could not start because passcode is not set on the device."
+            case .userFallback:
+                return "Authentication was canceled because the user tapped the fallback button (Enter Password)."
+            case .watchNotAvailable:
+                return "An attempt to authenticate with Apple Watch failed."
             default:
-                return "Authentication failed for unknown reason."
+                return "Authentication failed with reason code \(code.rawValue)."
         }
     }
 }
