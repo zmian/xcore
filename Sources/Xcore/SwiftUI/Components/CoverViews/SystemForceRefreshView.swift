96f1fc3825fdcc0f942536f78d1040fbe3f2f7e4
Zeeshan Mian <zmian@me.com>
2022-04-10 19:02:38 -0400

    add sys refresh view

---
+++ Sources/Xcore/SwiftUI/Components/CoverViews/SystemForceRefreshView.swift
@@ -0,0 +1,29 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+/// A view that shows the loading screen and asks ``AppStatusClient`` to force
+/// refresh.
+public struct SystemForceRefreshView: View {
+    @Environment(\.theme) private var theme
+    @Dependency(\.appStatus) private var appStatus
+
+    public init() {}
+
+    public var body: some View {
+        loadingView
+            .onAppear {
+                appStatus.systemForceRefresh()
+            }
+    }
+
+    private var loadingView: some View {
+        ProgressView()
+            .frame(max: .infinity)
+            .backgroundColor(theme.backgroundColor)
+    }
+}
