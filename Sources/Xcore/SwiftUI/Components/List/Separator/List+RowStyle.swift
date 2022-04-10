6c1207d173807a5a0985e06961027779c5241bb6
Zeeshan Mian <zmian@me.com>
2022-04-10 18:45:25 -0400

    Added separator

---
+++ Sources/Xcore/SwiftUI/Components/List/Separator/List+RowStyle.swift
@@ -0,0 +1,58 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension View {
+    public func listRowStyle() -> some View {
+        modifier(ListRowModifier())
+    }
+
+    public func listRowStyle(insets: EdgeInsets? = nil, separator separatorStyle: ListRowSeparatorStyle) -> some View {
+        self
+            .listRowStyle()
+            .listRowSeparatorStyle(separatorStyle)
+            .unwrap(insets) { content, insets in
+                content
+                    .customListRowInsets(insets)
+            }
+    }
+}
+
+// MARK: - ListRowModifier
+
+/// A structure to modify the row to add row insets and custom separator with
+/// full control over the separator insets as well.
+private struct ListRowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        InternalBody(content: content)
+    }
+
+    private struct InternalBody: View {
+        @Environment(\.theme) private var theme
+        @Environment(\.defaultMinListRowHeight) private var minHeight
+        @Environment(\.listRowSeparatorStyle) private var separatorStyle
+        @Environment(\.customListRowInsets) private var rowInsets
+        let content: Content
+
+        var body: some View {
+            content
+                .padding(rowInsets)
+                .frame(maxWidth: .infinity, minHeight: minHeight, alignment: .leading)
+                .listRowInsets(.zero)
+                .listRowBackground(Color.clear)
+                .apply {
+                    if #available(iOS 15.0, *) {
+                        $0.listRowSeparator(.hidden)
+                    } else {
+                        $0
+                    }
+                }
+                .separator()
+                .contentShape(Rectangle())
+        }
+    }
+}
