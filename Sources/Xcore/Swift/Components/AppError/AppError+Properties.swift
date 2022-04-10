07b787d5e997f1612d841077e4a63d2af8439781
Zeeshan Mian <zmian@me.com>
2022-04-10 19:50:18 -0400

    error helpers

---
+++ Sources/Xcore/Swift/Components/AppError/AppError+Properties.swift
@@ -0,0 +1,69 @@
+//
+// Xcore
+// Copyright © 2017 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension UserInfoKey where Type == AppError {
+    /// A boolean property indicating whether the error should show "Contact
+    /// Support" option.
+    public static var contactSupport: Self { #function }
+
+    /// A boolean property indicating whether the error should show "Open App
+    /// Settings" option.
+    public static var openAppSettings: Self { #function }
+}
+
+extension AppError {
+    /// A boolean property indicating whether the error should show "Contact
+    /// Support" option.
+    public var contactSupport: Bool {
+        get { self[userInfoKey: .contactSupport, default: false] }
+        set { self[userInfoKey: .contactSupport] = newValue }
+    }
+
+    /// A boolean property indicating whether the error should show "Open App
+    /// Settings" option.
+    public var openAppSettings: Bool {
+        get { self[userInfoKey: .openAppSettings, default: false] }
+        set { self[userInfoKey: .openAppSettings] = newValue }
+    }
+}
+
+extension Binding where Value == AppError? {
+    /// A boolean property indicating whether the error should show "Contact
+    /// Support" option.
+    public var contactSupport: Bool {
+        wrappedValue?.contactSupport == true
+    }
+
+    /// A boolean property indicating whether the error should show "Open App
+    /// Settings" option.
+    public var openAppSettings: Bool {
+        wrappedValue?.openAppSettings == true
+    }
+}
+
+extension AppError {
+    public init(
+        id: String,
+        title: String,
+        message: String,
+        logLevel: LogLevel? = nil,
+        contactSupport: Bool,
+        openAppSettings: Bool = false
+    ) {
+        self.init(
+            id: id,
+            title: title,
+            message: message,
+            logLevel: logLevel,
+            userInfo: [
+                .contactSupport: contactSupport,
+                .openAppSettings: openAppSettings
+            ]
+        )
+    }
+}
