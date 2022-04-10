fa92a26ca9057770c7d981797beaf3531a2af735
Zeeshan Mian <zmian@me.com>
2022-04-10 19:48:26 -0400

    Added debug

--- Sources/Xcore/Swift/Helpers/Functions.swift
+++ Sources/Xcore/Swift/Helpers/Functions.swift
@@ -25,3 +25,39 @@ public func ifDebug(_ work: () -> Void) {
     work()
     #endif
 }
+
+/// Prints items in debug mode and only when debugger is attached.
+@inline(__always)
+public func debugLog(
+    _ items: Any...,
+    info: String? = nil,
+    compact: Bool = false,
+    file: StaticString = #fileID,
+    line: UInt = #line
+) {
+    #if DEBUG
+    if AppInfo.isDebuggerAttached {
+        let info = info.map { " \($0)" } ?? ""
+        let firstLine = "[\(file):\(line)]\(info)"
+
+        if compact, items.count == 1 {
+            print("\(firstLine) \(items[0])")
+        } else {
+            print("———")
+            print(firstLine)
+            let isError = items.contains { ($0 as? Error) != nil }
+
+            if isError {
+                dump(items)
+            } else if items.count == 1 {
+                // Avoid printing array brackets.
+                print(items[0])
+            } else {
+                print(items)
+            }
+
+            print("———")
+        }
+    }
+    #endif
+}
