ee2b99f7dffb323e2ba6fa1931135303a083ca0f
Zeeshan Mian <zmian@me.com>
2022-06-17 16:15:00 -0400

    alignment

--- Sources/Xcore/SwiftUI/Components/FillView.swift
+++ Sources/Xcore/SwiftUI/Components/FillView.swift
@@ -11,15 +11,28 @@ import SwiftUI
 /// It's useful when you want to set background to take a over all the available
 /// space.
 public struct FillView<Content: View>: View {
+    private let alignment: Alignment
     private let content: Content
 
-    public init(@ViewBuilder content: () -> Content) {
+    public init(
+        alignment: Alignment = .center,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.alignment = alignment
+        self.content = content()
+    }
+
+    public init(
+        alignment: Alignment = .center,
+        _ content: @autoclosure @escaping () -> Content
+    ) {
+        self.alignment = alignment
         self.content = content()
     }
 
     public var body: some View {
         Color.clear
             .frame(max: .infinity)
-            .overlay(content)
+            .overlay(content, alignment: alignment)
     }
 }
