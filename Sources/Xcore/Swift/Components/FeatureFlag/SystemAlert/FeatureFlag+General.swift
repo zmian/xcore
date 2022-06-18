5d37cb5a2726b1a25cb41178756531557e2beb62
Zeeshan Mian <zmian@me.com>
2022-06-18 17:30:42 -0400

    rating prompt

--- Sources/Xcore/Swift/Components/FeatureFlag/SystemAlert/FeatureFlag+General.swift
+++ Sources/Xcore/Swift/Components/FeatureFlag/SystemAlert/FeatureFlag+General.swift
@@ -11,3 +11,26 @@ extension FeatureFlag {
         Key("one_time_code_character_limit").value(default: 6)
     }
 }
+
+// MARK: - App Store Rating Prompt
+
+extension FeatureFlag {
+    /// A boolean property indicating whether App Store rating prompt is enabled.
+    public static var ratingPromptEnabled: Bool {
+        key(#function).value()
+    }
+
+    /// A property indicating number of user visits multiple before auto prompting
+    /// the user the rating prompt.
+    ///
+    /// The default value is `10`, meaning after every 10th visit we ask the user to
+    /// rate the app. Apple API doesn't guarantee that user will see the prompt.
+    ///
+    /// - Note: If `ratingPromptEnabled` is `false` this property has no effect.
+    ///
+    /// If this property value is `0` then it means to never show the rating prompt
+    /// based on user's visits.
+    public static var ratingPromptVisitMultipleOf: Int {
+        key(#function).value(default: 10)
+    }
+}
