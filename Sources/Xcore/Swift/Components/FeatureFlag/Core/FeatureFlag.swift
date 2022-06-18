e729cfa3994e1dfad2a75d27b3eeb5dd95d8954b
Zeeshan Mian <zmian@me.com>
2022-06-18 17:29:59 -0400

    added debug

--- Sources/Xcore/Swift/Components/FeatureFlag/Core/FeatureFlag.swift
+++ Sources/Xcore/Swift/Components/FeatureFlag/Core/FeatureFlag.swift
@@ -14,4 +14,23 @@ import Foundation
 /// their functionality as classes or structures that extend this enumeration.
 /// For example, the **System Alert** flag returns a `SystemAlertConfiguration`
 /// instance.
-public enum FeatureFlag {}
+public enum FeatureFlag {
+    /// **Usage**
+    ///
+    /// ```swift
+    /// if FeatureFlag.someFeatureEnabled {
+    ///     // turn on feature
+    /// }
+    /// ```
+    ///
+    /// - Note: The `rawValue` is automatically transformed to snakecased.
+    public static func key(_ rawValue: String) -> Key {
+        .init(rawValue: rawValue.snakecased())
+    }
+
+    #if DEBUG
+    public static func setDebug<T>(_ key: String, value: T) {
+        ProcessInfo.Argument(rawValue: key).set(value)
+    }
+    #endif
+}
