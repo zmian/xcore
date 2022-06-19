d6052e14775c8a1ea683b810488621643315dc34
Zeeshan Mian <zmian@me.com>
2022-06-18 20:01:40 -0400

    separate file

---
+++ Sources/Xcore/SwiftUI/Extensions/Image+Extensions.swift
@@ -0,0 +1,25 @@
+//
+// Xcore
+// Copyright © 2022 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension Image {
+    /// Sets the mode by which SwiftUI resizes an image to fit its space.
+    ///
+    /// - Parameters:
+    ///   - isActive: A Boolean value that indicates whether image is resizable.
+    ///   - capInsets: Inset values that indicate a portion of the image that
+    ///     SwiftUI doesn't resize.
+    ///   - resizingMode: The mode by which SwiftUI resizes the image.
+    /// - Returns: An image, with the new resizing behavior set.
+    public func resizable(
+        isActive: Bool,
+        capInsets: EdgeInsets = EdgeInsets(),
+        resizingMode: Image.ResizingMode = .stretch
+    ) -> Image {
+        isActive ? resizable(capInsets: capInsets, resizingMode: resizingMode) : self
+    }
+}
