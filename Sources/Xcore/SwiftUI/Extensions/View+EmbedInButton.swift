aaf465fe6e34102a39a2f5df204c87ffa6dd6d41
Zeeshan Mian <zmian@me.com>
2022-06-18 23:07:48 -0400

    extn btn

---
+++ Sources/Xcore/SwiftUI/Extensions/View+EmbedInButton.swift
@@ -0,0 +1,44 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension View {
+    /// Embed this view in button if action isn't `nil`.
+    public func embedInButton(
+        if action: (() -> Void)?,
+        embeddedInList: Bool = true
+    ) -> some View {
+        modifier(EmbedInButtonViewModifier(
+            action: action,
+            embeddedInList: embeddedInList
+        ))
+    }
+}
+
+// MARK: - ViewModifier
+
+private struct EmbedInButtonViewModifier: ViewModifier {
+    @Environment(\.isLoading) private var isLoading
+    let action: (() -> Void)?
+    let embeddedInList: Bool
+
+    func body(content: Content) -> some View {
+        Group {
+            if let action = action {
+                Button(action: action) {
+                    content
+                }
+                .disabled(isLoading)
+            } else {
+                content
+            }
+        }
+        .applyIf(embeddedInList) {
+            $0.listRowStyle()
+        }
+    }
+}
