ad2ae623e7ed255f85b47e6a89a0ca1171ced400
Zeeshan Mian <zmian@me.com>
2022-06-18 20:15:09 -0400

    fitl btn

---
+++ Sources/Xcore/SwiftUI/Components/Button/FilterButton.swift
@@ -0,0 +1,40 @@
+//
+// Xcore
+// Copyright © 2021 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+public struct FilterButton: View {
+    @Environment(\.theme) private var theme
+    private let count: Int?
+    private let action: () -> Void
+
+    public init(_ count: Int? = nil, action: @escaping () -> Void) {
+        self.count = count
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: action) {
+            HStack(spacing: .s1) {
+                badge
+                Image(assetIdentifier: .filterIcon)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var badge: some View {
+        if let count = count, count > 0 {
+            Text("\(count)")
+                .font(.app(.footnote))
+                .foregroundColor(theme.backgroundColor)
+                .offset(y: -1)
+                .padding(.s2)
+                .backgroundColor(theme.tintColor)
+                .clipShape(Circle())
+        }
+    }
+}
