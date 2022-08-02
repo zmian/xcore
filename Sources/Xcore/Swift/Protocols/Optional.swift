b606e31a06c423a51eeb0af8089b85e087a0e62a
Zeeshan Mian <zmian@me.com>
2022-08-01 22:40:44 -0400

    Renamed `OptionalType` to `OptionalProtocol`.

--- Sources/Xcore/Swift/Protocols/Optional.swift
+++ Sources/Xcore/Swift/Protocols/Optional.swift
@@ -31,16 +31,16 @@ extension Optional: OptionalTypeMarker {
     }
 }
 
-// MARK: - OptionalType
+// MARK: - OptionalProtocol
 
 // Credit: https://stackoverflow.com/a/45462046
 
-public protocol OptionalType {
+public protocol OptionalProtocol {
     associatedtype Wrapped
     var wrapped: Wrapped? { get }
 }
 
-extension Optional: OptionalType {
+extension Optional: OptionalProtocol {
     public var wrapped: Wrapped? {
         self
     }
