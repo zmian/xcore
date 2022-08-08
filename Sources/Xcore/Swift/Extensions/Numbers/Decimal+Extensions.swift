932bb716ac55af5051fc19d4ce1526b6a5a3b9d5
Zeeshan Mian <zmian@me.com>
2022-08-07 21:36:05 -0400

    Added asNumber and asRounded

--- Sources/Xcore/Swift/Extensions/Numbers/Decimal+Extensions.swift
+++ Sources/Xcore/Swift/Extensions/Numbers/Decimal+Extensions.swift
@@ -314,26 +314,3 @@ extension Decimal {
         doubleValue ?? 0
     }
 }
-
-// MARK: - Formatted
-
-extension Decimal {
-    private static let formatter = NumberFormatter().apply {
-        $0.numberStyle = .decimal
-        $0.maximumFractionDigits = .maxFractionDigits
-    }
-
-    @available(iOS, introduced: 14, deprecated: 15, message: "Use .formattedString() directly.")
-    public func formattedString() -> String {
-        Self.formatter.string(from: self) ?? ""
-    }
-
-//    @available(iOS 15.0, *)
-//    public func formattedString() -> String {
-//        let style = FormatStyle
-//            .number
-//            .precision(.fractionLength(0...Int.maxFractionDigits))
-//
-//        return formatted(style)
-//    }
-}
