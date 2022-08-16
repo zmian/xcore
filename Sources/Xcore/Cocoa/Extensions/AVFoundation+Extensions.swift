e489e5eceac230961eb1982714b20bfaaf968950
Zeeshan Mian <zmian@me.com>
2022-08-16 16:40:59 -0400

    AVPlayer: Added `currentTime`

--- Sources/Xcore/Cocoa/Extensions/AVFoundation+Extensions.swift
+++ Sources/Xcore/Cocoa/Extensions/AVFoundation+Extensions.swift
@@ -6,6 +6,38 @@
 
 import AVFoundation
 
+// MARK: - Current Playback Time monitoring
+
+extension AVPlayer {
+    /// A Boolean property indicating whether playback is in progress.
+    public var isPlaying: Bool {
+        timeControlStatus == .playing
+    }
+
+    public func currentTime(_ block: @escaping (_ seconds: TimeInterval, _ formattedTime: String) -> Void) -> Any {
+        let interval = CMTime(value: 1, timescale: 1)
+
+        return addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] _ in
+            guard let strongSelf = self else { return }
+            let currentTime = strongSelf.currentTime()
+            let normalizedTime = Double(currentTime.value) / Double(currentTime.timescale)
+            block(normalizedTime, strongSelf.format(seconds: normalizedTime))
+        }
+    }
+
+    private func format(seconds: TimeInterval) -> String {
+        let sec = seconds % 60
+        let min = seconds / 60
+        let hrs = seconds / 3600
+
+        if hrs == 0 {
+            return String(format: "%02d:%02d", min, sec)
+        }
+
+        return String(format: "%02d:%02d:%02d", hrs, min, sec)
+    }
+}
+
 // MARK: - AVPlayerItem
 
 extension AVPlayerItem {
