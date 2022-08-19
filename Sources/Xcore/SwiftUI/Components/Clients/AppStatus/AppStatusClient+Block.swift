e113dd4e684954c423e8e0267e5b7c3147e49205
Zeeshan Mian <zmian@me.com>
2022-08-19 17:42:44 -0400

    Added BlockAppStatusClient

---
+++ Sources/Xcore/SwiftUI/Components/Clients/AppStatus/AppStatusClient+Block.swift
@@ -0,0 +1,66 @@
+//
+// Xcore
+// Copyright © 2022 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import Foundation
+
+public struct BlockAppStatusClient: AppStatusClient {
+    private let _evaluate: () -> Void
+    private let _changeSessionTo: (AppStatus.SessionState) -> Void
+    private let _systemForceRefresh: () -> Void
+    public let receive: ValuePublisher<AppStatus, Never>
+    public let sessionState: ValuePublisher<AppStatus.SessionState, Never>
+
+    /// Creates an app status client.
+    ///
+    /// - Parameters:
+    ///   - receive: Emit events when the app status changes.
+    ///   - sessionState: Emit events when the app session state changes.
+    ///   - evaluate: App status client to evaluate the app status.
+    ///   - changeSessionTo: Transition the app to given session state.
+    ///   - systemForceRefresh: Purge app cache, save refresh hash and
+    ///     re-evaluate the app state. Warning: Do not purge any data that would
+    ///     force the user to sign in again.
+    public init(
+        receive: ValuePublisher<AppStatus, Never>,
+        sessionState: ValuePublisher<AppStatus.SessionState, Never>,
+        evaluate: @escaping () -> Void,
+        changeSessionTo: @escaping (AppStatus.SessionState) -> Void,
+        systemForceRefresh: @escaping () -> Void
+    ) {
+        self.receive = receive
+        self.sessionState = sessionState
+        self._evaluate = evaluate
+        self._changeSessionTo = changeSessionTo
+        self._systemForceRefresh = systemForceRefresh
+    }
+
+    public func evaluate() {
+        _evaluate()
+    }
+
+    public func changeSession(to state: AppStatus.SessionState) {
+        _changeSessionTo(state)
+    }
+
+    public func systemForceRefresh() {
+        _systemForceRefresh()
+    }
+}
+
+// MARK: - Variants
+
+extension AppStatusClient where Self == BlockAppStatusClient {
+    /// Returns noop variant of `BlockAppStatusClient`.
+    public static var noop: Self {
+        .init(
+            receive: .constant(.preparingLaunch),
+            sessionState: .constant(.signedOut),
+            evaluate: {},
+            changeSessionTo: { _ in },
+            systemForceRefresh: {}
+        )
+    }
+}
