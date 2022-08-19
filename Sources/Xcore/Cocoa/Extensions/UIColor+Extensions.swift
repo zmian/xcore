dc5292a142f6e8512254ebc96ff6b36a5f07cc5c
Zeeshan Mian <zmian@me.com>
2022-08-19 00:54:59 -0400

    Color ext to bring lighter and darker functions over.

--- Sources/Xcore/Cocoa/Extensions/UIColor+Extensions.swift
+++ Sources/Xcore/Cocoa/Extensions/UIColor+Extensions.swift
@@ -200,44 +200,6 @@ extension UIColor {
     }
 }
 
-// MARK: - Cross Fade
-
-extension UIColor {
-    /// Returns a transition color from `self` to the given color based on the given
-    /// delta.
-    ///
-    /// - Parameters:
-    ///   - color: The color to which self should cross fade.
-    ///   - percentage: The delta of the cross fade.
-    /// - Returns: An instance of cross faded `UIColor`.
-    open func crossFade(to color: UIColor, delta percentage: CGFloat) -> UIColor {
-        let fromColor = self
-        let toColor = color
-
-        var fromRed: CGFloat = 0
-        var fromGreen: CGFloat = 0
-        var fromBlue: CGFloat = 0
-        var fromAlpha: CGFloat = 0
-
-        fromColor.getRed(&fromRed, green: &fromGreen, blue: &fromBlue, alpha: &fromAlpha)
-
-        var toRed: CGFloat = 0
-        var toGreen: CGFloat = 0
-        var toBlue: CGFloat = 0
-        var toAlpha: CGFloat = 0
-
-        toColor.getRed(&toRed, green: &toGreen, blue: &toBlue, alpha: &toAlpha)
-
-        // Calculate the actual RGBA values of the fade colour
-        let red = (toRed - fromRed) * percentage + fromRed
-        let green = (toGreen - fromGreen) * percentage + fromGreen
-        let blue = (toBlue - fromBlue) * percentage + fromBlue
-        let alpha = (toAlpha - fromAlpha) * percentage + fromAlpha
-
-        return UIColor(.default, red: red, green: green, blue: blue, alpha: alpha)
-    }
-}
-
 // MARK: - Color Scheme Mode
 
 extension UIColor {
