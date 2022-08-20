ab44e72c7fe08c6f9c0b326e6a2325aca648f746
Zeeshan Mian <zmian@me.com>
2022-08-19 23:35:52 -0400

    lint

--- Example/App.swift
+++ Example/App.swift
@@ -8,24 +8,14 @@ import SwiftUI
 
 @main
 struct XcoreApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    init() {
+        SwizzleManager.start()
+        Theme.start()
+    }
 
     var body: some Scene {
         WindowGroup {
             RootView()
         }
-    }
-}
-
-// MARK: - Delegate
-
-private final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        UIApplication.swizzle()
-        Theme.start()
-        return true
     }
 }
