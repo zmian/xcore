86c10b94fb71c491cd17e336b9d299ba7889b115
Zeeshan Mian <zmian@me.com>
2022-08-19 23:30:07 -0400

    lint

--- Sources/Xcore/SwiftUI/Components/Spacer.swift
+++ Sources/Xcore/SwiftUI/Components/Spacer.swift
@@ -6,11 +6,15 @@
 
 import SwiftUI
 
+/// A flexible space that expands along the major axis of its containing stack
+/// layout, or on both axes if not contained in a stack.
 public func Spacer(height: CGFloat) -> some View {
     Spacer()
         .frame(height: height)
 }
 
+/// A flexible space that expands along the major axis of its containing stack
+/// layout, or on both axes if not contained in a stack.
 public func Spacer(width: CGFloat) -> some View {
     Spacer()
         .frame(width: width)
