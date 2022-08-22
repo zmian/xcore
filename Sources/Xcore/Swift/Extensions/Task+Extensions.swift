0373c5d7d7b30f8139ee33a9a5e1010e7d478c71
Zeeshan Mian <zmian@me.com>
2022-08-22 15:38:53 -0400

    Task extensions

---
+++ Sources/Xcore/Swift/Extensions/Task+Extensions.swift
@@ -0,0 +1,20 @@
+//
+// Xcore
+// Copyright © 2022 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+extension Task where Success == Never, Failure == Never {
+    /// Suspends the current task for at least the given duration in seconds.
+    ///
+    /// If the task is canceled before the time ends, this function throws
+    /// `CancellationError`.
+    ///
+    /// This function doesn't block the underlying thread.
+    public static func sleep(seconds duration: TimeInterval) async throws {
+        try await sleep(nanoseconds: UInt64(TimeInterval(NSEC_PER_SEC) * duration))
+    }
+}
