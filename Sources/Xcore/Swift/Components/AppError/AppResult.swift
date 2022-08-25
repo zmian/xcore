79fd49974dd2a1d251824b90bbca33c9ba3fb4bd
Zeeshan Mian <zmian@me.com>
2022-08-25 01:42:43 -0400

    Async: Added few helper methods for AppResult

--- Sources/Xcore/Swift/Components/AppError/AppResult.swift
+++ Sources/Xcore/Swift/Components/AppError/AppResult.swift
@@ -8,4 +8,43 @@ import Foundation
 
 /// A value that represents either a success or a failure as an `AppError`,
 /// including an associated value in each case.
-public typealias AppResult<Value> = Swift.Result<Value, AppError>
+public typealias AppResult<Value> = Result<Value, AppError>
+
+// MARK: - Result Helpers
+
+extension Result where Failure == AppError {
+    /// Creates a new result by evaluating an async throwing closure, capturing the
+    /// returned value as a success, or any thrown error as an `AppError`.
+    ///
+    /// Any thrown error is attempted to cast to app error; otherwise
+    /// `AppError.general` failure value is used.
+    ///
+    /// - Parameter body: An async throwing closure to evaluate.
+    @_transparent
+    public init(catching body: @Sendable () async throws -> Success) async {
+        do {
+            self = .success(try await body())
+        } catch {
+            self = .failure((error as? AppError) ?? .general)
+        }
+    }
+}
+
+extension Result where Failure == AppError, Success == Xcore.Empty {
+    /// Creates a new result by evaluating an async throwing closure, capturing the
+    /// returned value as a success, or any thrown error as an `AppError`.
+    ///
+    /// Any thrown error is attempted to cast to app error; otherwise
+    /// `AppError.general` failure value is used.
+    ///
+    /// - Parameter body: An async throwing closure to evaluate.
+    @_transparent
+    public init(catching body: @Sendable () async throws -> Void) async {
+        do {
+            _ = try await body()
+            self = .success
+        } catch {
+            self = .failure((error as? AppError) ?? .general)
+        }
+    }
+}
