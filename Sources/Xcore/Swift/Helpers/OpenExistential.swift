a0af8b0f492657b4ac46da4aec09f5c51e48e80b
Zeeshan Mian <zmian@me.com>
2022-09-21 19:54:44 -0400

    OpenExistential: Using the `any` keyword from Swift 5.7 to simplify the implementation.

--- Sources/Xcore/Swift/Helpers/OpenExistential.swift
+++ Sources/Xcore/Swift/Helpers/OpenExistential.swift
@@ -30,61 +30,21 @@ extension Mirror {
     /// - Parameter value: The value to check if it is a type of a collection
     ///   and the collection is empty.
     public static func isEmpty(_ value: Any) -> Bool? {
-        _isEmpty(value)
+        (value as? any Collection)?.isEmpty
     }
 }
 
-// MARK: - Helpers
+// MARK: - Equatable.isEqual
 
 // Credit to TCA for this Approach:
 // https://github.com/pointfreeco/swift-composable-architecture/blob/108e3a536fcebb16c4f247ef92c2d7326baf9fe3/Sources/ComposableArchitecture/Effects/TaskResult.swift#L284-L305
-private enum _Witness<A> {}
 
-// MARK: - AnyCollection.isEmpty
-
-private protocol _AnyCollection {
-    static func _isEmpty(_ value: Any) -> Bool
-}
-
-extension _Witness: _AnyCollection where A: Collection {
-    static func _isEmpty(_ value: Any) -> Bool {
-        guard let value = value as? A else {
-            return false
-        }
-
-        return value.isEmpty
+extension Equatable {
+    fileprivate func _isEqual(other: Any) -> Bool {
+        self == other as? Self
     }
 }
 
-private func _isEmpty(_ value: Any) -> Bool? {
-    func `do`<A>(_: A.Type) -> Bool? {
-        (_Witness<A>.self as? _AnyCollection.Type)?._isEmpty(value)
-    }
-    return _openExistential(type(of: value), do: `do`)
-}
-
-// MARK: - AnyEquatable.isEqual
-
-private protocol _AnyEquatable {
-    static func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool
-}
-
-extension _Witness: _AnyEquatable where A: Equatable {
-    static func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
-        guard
-            let lhs = lhs as? A,
-            let rhs = rhs as? A
-        else {
-            return false
-        }
-
-        return lhs == rhs
-    }
-}
-
-private func _isEqual(_ a: Any, _ b: Any) -> Bool? {
-    func `do`<A>(_: A.Type) -> Bool? {
-        (_Witness<A>.self as? _AnyEquatable.Type)?._isEqual(a, b)
-    }
-    return _openExistential(type(of: a), do: `do`)
+private func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool? {
+    (lhs as? any Equatable)?._isEqual(other: rhs)
 }
