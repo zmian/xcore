6149cfbb9143170f231ac84447f03ccdab237420
Zeeshan Mian <zmian@me.com>
2022-10-10 15:57:57 -0400

    Adde device shake modifier

---
+++ Sources/Xcore/SwiftUI/Extensions/View+Shake.swift
@@ -0,0 +1,53 @@
+//
+// Xcore
+// Copyright © 2022 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+extension View {
+    /// Adds an action to perform when the user shakes the device.
+    ///
+    /// - Parameter action: The action to perform when the user shakes the device.
+    ///
+    /// - Returns: A view that triggers `action` the user shakes the device.
+    fileprivate func onDeviceShake(perform action: @escaping (() -> Void)) -> some View {
+        modifier(DeviceShakeViewModifier(perform: action))
+    }
+}
+
+// MARK: - ViewModifier
+
+/// A view modifier that detects user shaking the device and calls the given
+/// action closure.
+private struct DeviceShakeViewModifier: ViewModifier {
+    private let action: (() -> Void)
+
+    init(perform action: @escaping (() -> Void)) {
+        self.action = action
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .task {
+                for await _ in NotificationCenter.async(UIDevice.userDidShakeDeviceNotification) {
+                    action()
+                }
+            }
+    }
+}
+
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        if motion == .motionShake {
+            NotificationCenter.post(UIDevice.userDidShakeDeviceNotification)
+        }
+    }
+}
+
+extension UIDevice {
+    /// A notification that posts when the user shakes the device.
+    fileprivate static let userDidShakeDeviceNotification = Notification.Name(rawValue: "xcore.userDidShakeDeviceNotification")
+}
