18d5925242c2d1cc7f7d127a9d27041fe7db79c6
Zeeshan Mian <zmian@me.com>
2023-03-05 16:53:30 -0500

    Reverts 1721e831: back to struct to avoid complicating the interfact.

--- Sources/Xcore/Swift/Dependencies/AppStatus/AppStatusClient.swift
+++ Sources/Xcore/Swift/Dependencies/AppStatus/AppStatusClient.swift
@@ -7,24 +7,66 @@
 import SwiftUI
 
 /// Provides functionality for evaluating and receiving events for app’s state.
-public protocol AppStatusClient {
+public struct AppStatusClient {
     /// Receive the app status events.
-    var receive: ValuePublisher<AppStatus, Never> { get }
+    public let receive: ValuePublisher<AppStatus, Never>
 
     /// Receive session state change events.
-    var sessionState: ValuePublisher<AppStatus.SessionState, Never> { get }
+    public let sessionState: ValuePublisher<AppStatus.SessionState, Never>
 
     /// Asks the app status client to evaluate the status.
-    func evaluate()
+    private let _evaluate: () -> Void
 
     /// Transition the app to given session state.
-    func changeSession(to state: AppStatus.SessionState)
+    private let _changeSessionTo: (AppStatus.SessionState) -> Void
 
     /// A method to purge cache (e.g., remote images), save the hash and re-evaluate
     /// the app state.
     ///
     /// - Warning: Do not purge any data that would force the user to sign in again.
-    func systemForceRefresh()
+    private let _systemForceRefresh: () -> Void
+
+    /// Creates an app status client.
+    ///
+    /// - Parameters:
+    ///   - receive: Emit events when the app status changes.
+    ///   - sessionState: Emit events when the app session state changes.
+    ///   - evaluate: App status client to evaluate the app status.
+    ///   - changeSessionTo: Transition the app to given session state.
+    ///   - systemForceRefresh: Purge app cache, save refresh hash and
+    ///     re-evaluate the app state. Warning: Do not purge any data that would
+    ///     force the user to sign in again.
+    public init(
+        receive: ValuePublisher<AppStatus, Never>,
+        sessionState: ValuePublisher<AppStatus.SessionState, Never>,
+        evaluate: @escaping () -> Void,
+        changeSessionTo: @escaping (AppStatus.SessionState) -> Void,
+        systemForceRefresh: @escaping () -> Void
+    ) {
+        self.receive = receive
+        self.sessionState = sessionState
+        self._evaluate = evaluate
+        self._changeSessionTo = changeSessionTo
+        self._systemForceRefresh = systemForceRefresh
+    }
+
+    /// Asks the app status client to evaluate the status.
+    public func evaluate() {
+        _evaluate()
+    }
+
+    /// Transition the app to given session state.
+    public func changeSession(to state: AppStatus.SessionState) {
+        _changeSessionTo(state)
+    }
+
+    /// A method to purge cache (e.g., remote images), save the hash and re-evaluate
+    /// the app state.
+    ///
+    /// - Warning: Do not purge any data that would force the user to sign in again.
+    public func systemForceRefresh() {
+        _systemForceRefresh()
+    }
 }
 
 // MARK: - Helpers
@@ -44,6 +86,38 @@ extension AppStatusClient {
         let isApplicationActive = UIApplication.sharedOrNil?.applicationState != .background
         let isProtectedDataAvailable = UIApplication.shared.isProtectedDataAvailable
         return isApplicationActive && isProtectedDataAvailable && sessionState.value == .unlocked
+    }
+}
+
+// MARK: - Variants
+
+extension AppStatusClient {
+    /// Returns noop variant of `AppStatusClient`.
+    public static var noop: Self {
+        .init(
+            receive: .constant(.preparingLaunch),
+            sessionState: .constant(.signedOut),
+            evaluate: {},
+            changeSessionTo: { _ in },
+            systemForceRefresh: {}
+        )
+    }
+
+    /// Returns unimplemented variant of `AppStatusClient`.
+    public static var unimplemented: Self {
+        .init(
+            receive: .constant(.preparingLaunch),
+            sessionState: .constant(.signedOut),
+            evaluate: {
+                XCTFail("\(Self.self).evaluate is unimplemented")
+            },
+            changeSessionTo: { _ in
+                XCTFail("\(Self.self).changeSessionTo is unimplemented")
+            },
+            systemForceRefresh: {
+                XCTFail("\(Self.self).systemForceRefresh is unimplemented")
+            }
+        )
     }
 }
 
