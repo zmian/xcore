15b2ceeaafd21bcd8f5b2ed7d3101dfbd1b22742
Zeeshan Mian <zmian@me.com>
2023-03-04 22:59:06 -0500

    Refactored `SessionCounterClient` to be struct away from protocol.

--- Sources/Xcore/Swift/Dependencies/SessionCounter/SessionCounterClient.swift
+++ Sources/Xcore/Swift/Dependencies/SessionCounter/SessionCounterClient.swift
@@ -7,12 +7,48 @@
 import Foundation
 
 /// Provides functionality to keep track of user's session counts.
-public protocol SessionCounterClient {
-    /// Returns user's current session count.
-    var count: Int { get }
+public struct SessionCounterClient {
+    private let _count: @Sendable () -> Int
 
     /// Increments user's session count.
-    func increment()
+    public let increment: @Sendable () -> Void
+
+    /// Creates a client to keep track of user's session counts.
+    ///
+    /// - Parameters:
+    ///   - count: Returns user's current session count.
+    ///   - increment: Increments user's session count.
+    public init(
+        count: @escaping @Sendable() -> Int,
+        increment: @escaping @Sendable () -> Void
+    ) {
+        self._count = count
+        self.increment = increment
+    }
+
+    /// Returns user's current session count.
+    public var count: Int {
+        _count()
+    }
+}
+
+// MARK: - Variants
+
+extension SessionCounterClient {
+    /// Returns noop variant of `SessionCounterClient`.
+    public static var noop: Self {
+        .init { 0 } increment: {}
+    }
+
+    /// Returns unimplemented variant of `SessionCounterClient`.
+    public static var unimplemented: Self {
+        .init {
+            XCTFail("\(Self.self).count is unimplemented")
+            return 0
+        } increment: {
+            XCTFail("\(Self.self).increment is unimplemented")
+        }
+    }
 }
 
 // MARK: - Dependency
@@ -20,6 +56,8 @@ public protocol SessionCounterClient {
 extension DependencyValues {
     private struct SessionCounterClientKey: DependencyKey {
         static var liveValue: SessionCounterClient = .live
+        static var testValue: SessionCounterClient = .unimplemented
+        static var previewValue: SessionCounterClient = .noop
     }
 
     /// Provides functionality to keep track of user's session counts.
