b4f2b073d6e591af940d3deab5fa0fe63622d3f7
Zeeshan Mian <zmian@me.com>
2023-08-04 20:19:06 -0400

    Updated `swift-dependencies` to `1.0.0`

--- Package.swift
+++ Package.swift
@@ -9,10 +9,12 @@ let package = Package(
         .library(name: "Xcore", targets: ["Xcore"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2"),
+        // Manually doing 5.12.6, as "5.13.0" has memory leaks that consumes 50% more memory.
+        // Once, future versions are fixed we can then upgrade.
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.12.6"),
         .package(url: "https://github.com/zmian/AnyCodable", branch: "master"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.3")
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0")
     ],
     targets: [
         .target(
