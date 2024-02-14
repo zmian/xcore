75bf169868321dc0aa9b4e0cc7fdb0cc98d4453b
Zeeshan Mian <zmian@me.com>
2024-02-14 20:52:26 +1100

    Updated build script

--- Scripts/build_docs.sh
+++ Scripts/build_docs.sh
@@ -1,18 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
-if ! which jazzy >/dev/null; then
-  echo "Jazzy not detected: You can download it from https://github.com/realm/jazzy"
-  exit
-fi
-
-jazzy \
-  --clean \
-  --author Zeeshan Mian \
-  --author_url https://zmian.me \
-  --github_url https://github.com/zmian/xcore.swift \
-  --github-file-prefix https://github.com/zmian/xcore.swift/blob/master \
-  --module-version 1.0.0 \
-  --xcodebuild-arguments -scheme, Xcore \
-  --module Xcore \
-  --root-url https://github.com/zmian/xcore.swift\
-  -x -workspace,Xcore.xcworkspace,-scheme,Xcore
+swift package \
+    --allow-writing-to-directory ./docs \
+    generate-documentation \
+    --target Xcore \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path Xcore \
+    --output-path ./docs
