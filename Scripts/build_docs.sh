63ba0a8e547d495be636e009f3abfca1c8f4b6d9
Zeeshan Mian <zmian@me.com>
2024-02-14 21:16:39 +1100

    updated

--- Scripts/build_docs.sh
+++ Scripts/build_docs.sh
@@ -1,10 +1,20 @@
 #!/bin/zsh
 
-swift package \
-    --allow-writing-to-directory ./docs \
-    generate-documentation \
-    --target Xcore \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path Xcore \
-    --output-path ./docs
+# swift package \
+#     --allow-writing-to-directory ./docs \
+#     generate-documentation \
+#     --target Xcore \
+#     --disable-indexing \
+#     --transform-for-static-hosting \
+#     --hosting-base-path Xcore \
+#     --output-path ./docs
+
+# TODO: Finish setting up DocC
+# https://maxxfrazer.medium.com/deploying-docc-with-github-actions-218c5ca6cad5
+# https://medium.com/kinandcartacreated/host-and-automate-your-docc-documentation-c6ac29dc0feb
+xcodebuild docbuild \
+    -derivedDataPath docc \
+    -workspace Xcore.xcworkspace \
+    -scheme Xcore \
+    -destination 'generic/platform=iOS' \
+    -parallelizeTargets
