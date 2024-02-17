9affceaf7d708c517099918c8e55288f0729ac9a
Zeeshan Mian <zmian@me.com>
2024-02-18 00:40:38 +1100

    lint

--- Sources/Xcore/Swift/Components/DatesTimers/Timer/Timer+Extensions.swift
+++ Sources/Xcore/Swift/Components/DatesTimers/Timer/Timer+Extensions.swift
@@ -44,36 +44,6 @@ extension Timer {
     /// ```swift
     /// var count = 0
     ///
-    /// Timer.every(5) {
-    ///     count += 1
-    ///     print("Hi, count: \(count)")
-    /// }
-    /// ```
-    ///
-    /// - Parameters:
-    ///    - interval: The number of seconds between firings of the timer. If
-    ///      interval is less than or equal to `0.0`, this method chooses the
-    ///      nonnegative value of `0.0001` seconds instead.
-    ///    - work: A closure to be executed when the timer fires.
-    /// - Returns: A new Timer object, configured according to the specified
-    ///   parameters.
-    @discardableResult
-    public class func every(_ interval: TimeInterval, _ work: @escaping () -> Void) -> Timer {
-        Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
-            work()
-        }
-    }
-
-    /// Creates a repeating timer and schedules it on the current run loop in the
-    /// default mode.
-    ///
-    /// After interval seconds have elapsed, the timer fires, executing block.
-    ///
-    /// **Usage**
-    ///
-    /// ```swift
-    /// var count = 0
-    ///
     /// Timer.every(5) { timer in
     ///     count += 1
     ///     print("Hi, count: \(count)")
@@ -93,7 +63,10 @@ extension Timer {
     /// - Returns: A new Timer object, configured according to the specified
     ///   parameters.
     @discardableResult
-    public class func every(_ interval: TimeInterval, _ work: @escaping (Timer) -> Void) -> Timer {
+    public class func every(
+        _ interval: TimeInterval,
+        _ work: @escaping (Timer) -> Void
+    ) -> Timer {
         Timer.scheduledTimer(withTimeInterval: interval, repeats: true) {
             work($0)
         }
@@ -136,6 +109,9 @@ extension Timer {
             return
         }
 
-        fireDate = Date(timeInterval: -pauseDate.timeIntervalSinceNow, since: previousFireDate)
+        fireDate = Date(
+            timeInterval: -pauseDate.timeIntervalSinceNow,
+            since: previousFireDate
+        )
     }
 }
