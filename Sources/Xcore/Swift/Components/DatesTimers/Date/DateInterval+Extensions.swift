83e11983e87b479f60d5cfbefa5b93893dc0ae0d
Zeeshan Mian <zmian@me.com>
2024-02-17 23:30:44 +1100

    lint

--- Sources/Xcore/Swift/Components/DatesTimers/Date/DateInterval+Extensions.swift
+++ Sources/Xcore/Swift/Components/DatesTimers/Date/DateInterval+Extensions.swift
@@ -7,14 +7,14 @@
 import Foundation
 
 extension DateInterval {
-    /// Returns the middle Date of a DateInterval
+    /// Returns the middle `Date` of a `DateInterval`.
     ///
     /// Due to timezone and daylight savings constraints, relying on either
-    /// `.start` or `.end` of a DateInterval - when the goal is to group days -
+    /// `.start` or `.end` of a `DateInterval` — when the goal is to group days —
     /// might produce unexpected results as they can return the previous or next
     /// day respectively.
     ///
-    /// With `.middle` we can be sure we'll always land on the correct day.
+    /// With `.middle`, we can be sure we'll always land on the correct day.
     var middle: Date {
         start + (duration / 2)
     }
