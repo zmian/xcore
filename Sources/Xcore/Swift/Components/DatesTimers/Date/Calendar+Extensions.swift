655f8039a2b7678476d6356bb1d2ff3f456961b3
Zeeshan Mian <zmian@me.com>
2024-02-17 23:33:37 +1100

    lint

--- Sources/Xcore/Swift/Components/DatesTimers/Date/Calendar+Extensions.swift
+++ Sources/Xcore/Swift/Components/DatesTimers/Date/Calendar+Extensions.swift
@@ -7,26 +7,26 @@
 import Foundation
 
 extension Calendar: MutableAppliable {
+    /// The default calendar, initially set to the current calendar.
     public static var `default`: Self = .current
 
     /// The default calendar used in ``DateCodingFormatStyle``.
     public static var defaultCodable: Self = .iso
 
-    /// Returns `ISO` calendar with `en_US_POSIX` locale and `UTC` time zone.
-    public static let iso = Self(
-        identifier: .gregorian
-    ).applying {
+    /// Returns the `ISO` calendar with `en_US_POSIX` locale and `UTC` time zone.
+    public static let iso = Self(identifier: .gregorian).applying {
         $0.timeZone = .utc
         $0.locale = .usPosix
     }
 }
 
 extension Calendar {
-    /// Returns `gregorian` calendar with given locale and time zone.
+    /// Returns the `gregorian` calendar with the given time zone and locale.
     ///
     /// - Parameters:
     ///   - timeZone: The time zone of the calendar.
     ///   - locale: The locale of the calendar.
+    /// - Returns: A `gregorian` calendar with the specified time zone and locale.
     public static func gregorian(timeZone: TimeZone, locale: Locale = .current) -> Self {
         Calendar(identifier: .gregorian).applying {
             $0.timeZone = timeZone
