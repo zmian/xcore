66c397cc75a46b1b6ab754d48b92faa352567e2a
Zeeshan Mian <zmian@me.com>
2024-02-19 11:29:19 +1100

    Added Example Watch App

---
+++ Watch/App.swift
@@ -0,0 +1,16 @@
+//
+// Xcore
+// Copyright © 2024 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+// Xcore
+// Copyright © 2024 Xcore
+// MIT license, see LICENSE file for details
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
