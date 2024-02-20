5646bcc614ed9e8d93ab99f95cfd41772671ebdd
Zeeshan Mian <zmian@me.com>
2024-02-20 16:05:49 +1100

    Xcode 15 finally fixes the `Bundle.module` so the workaround is no longer needed to run previews.

--- Sources/Xcore/Swift/Helpers/Xcore.swift
+++ Sources/Xcore/Swift/Helpers/Xcore.swift
@@ -15,7 +15,7 @@ extension Bundle {
     private class XcoreMarker {}
     public static var xcore: Bundle {
         #if SWIFT_PACKAGE
-        return .myModule
+        return .module
         #else
         return .init(for: XcoreMarker.self)
         #endif
@@ -27,55 +27,3 @@ extension AnyCodable {
         self.init(value)
     }
 }
-
-#if SWIFT_PACKAGE
-// Fix for package depending on other packages fix
-// =============================================================================
-#warning("TODO: Check previews under Xcode 14 to see if this is fixed?")
-private let moduleName = "Xcore"
-
-private class CurrentBundleFinder {}
-
-// Unable to find Bundle in package target tests or SwiftUI Previews when
-// package depends on another package containing resources accessed via
-// Bundle.module.
-//
-// - SeeAlso: https://forums.swift.org/t/unable-to-find-bundle-in-package-target-tests-when-package-depends-on-another-package-containing-resources-accessed-via-bundle-module/43974/2
-extension Foundation.Bundle {
-    /// Returns the resource bundle associated with the current Swift module.
-    fileprivate static var myModule: Bundle = {
-        let bundleName = "Xcore_\(moduleName)"
-        let localBundleName = "LocalPackages_\(moduleName)"
-
-        let candidates = [
-            // Bundle should be present here when the package is linked into an App.
-            Bundle.main.resourceURL,
-
-            // Bundle should be present here when the package is linked into a framework.
-            Bundle(for: CurrentBundleFinder.self).resourceURL,
-
-            // For command-line tools.
-            Bundle.main.bundleURL,
-
-            // Bundle should be present here when running previews from a different package (this is the path to "…/Debug-iphonesimulator/").
-            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent(),
-            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent()
-        ]
-
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-
-            let localBundlePath = candidate?.appendingPathComponent(localBundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-
-        fatalError("Unable to find bundle named \(moduleName).")
-    }()
-}
-// =============================================================================
-#endif
