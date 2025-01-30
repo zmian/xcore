6bb5fd6f235a70c59c16065c7e1bd6ca262eebd6
Zeeshan Mian <zmian@me.com>
2025-01-30 15:20:27 +1100

    Renamed `sessionExpiration` to `inactivityDuration` for clarity

--- Sources/Xcore/Swift/Components/Cache/SessionAwareCache.swift
+++ Sources/Xcore/Swift/Components/Cache/SessionAwareCache.swift
@@ -10,7 +10,7 @@ import Combine
 /// A session-aware caching mechanism for tracking values during app activity.
 ///
 /// This cache retains values only during an active session. If the app remains
-/// in the background for longer than the specified `sessionExpiration`, the
+/// in the background for longer than the specified `inactivityDuration`, the
 /// cache is cleared, marking the start of a new session.
 ///
 /// It is useful for tracking events that have already been sent to prevent
@@ -21,13 +21,13 @@ import Combine
 ///
 /// A session is defined as the period during which the app stays in the
 /// foreground. If the app is sent to the background for longer than the
-/// specified `sessionExpiration`, it is considered a new session, and all
+/// specified `inactivityDuration`, it is considered a new session, and all
 /// cached values are cleared.
 ///
 /// **Usage**
 ///
 /// ```swift
-/// let analyticsSessionCache = SessionAwareCache<String>(sessionExpiration: .seconds(30))
+/// let analyticsSessionCache = SessionAwareCache<String>(inactivityDuration: .seconds(30))
 /// let event = AppAnalyticsEvent(name: "example_event")
 /// if !analyticsSessionCache.insert(event.name) {
 ///     // Send to the analytics service
@@ -36,17 +36,17 @@ import Combine
 /// ```
 public final class SessionAwareCache<Value: Sendable & Hashable>: @unchecked Sendable {
     @Dependency(\.appPhase) private var appPhase
-    private let lastActiveUptime = SystemUptime()
-    private let sessionExpirationDuration: TimeInterval
+    private let lastActiveTime = SystemUptime()
+    private let inactivityDuration: TimeInterval
     private let storage = LockIsolated<Set<Value>>([])
     nonisolated(unsafe) private var cancellable: AnyCancellable?
 
     /// Creates an instance of `SessionAwareCache`.
     ///
-    /// - Parameter duration: The duration after which the session expires if the
-    ///   app remains in the background.
-    public init(sessionExpiration duration: Duration = .seconds(30)) {
-        self.sessionExpirationDuration = TimeInterval(duration.components.seconds)
+    /// - Parameter inactivityDuration: The duration after which the session expires
+    ///   if the app remains in the background.
+    public init(inactivityDuration: Duration = .seconds(30)) {
+        self.inactivityDuration = TimeInterval(inactivityDuration.components.seconds)
 
         withDelay(.seconds(0.3)) { [weak self] in
             // Delay to avoid:
@@ -59,18 +59,18 @@ public final class SessionAwareCache<Value: Sendable & Hashable>: @unchecked Sen
 
     /// Observes app lifecycle events to manage session activity.
     ///
-    /// When the app enters the background, the `lastActiveUptime` is recorded.
-    /// When the app returns to the foreground, the session expiration is checked.
-    /// If the session has expired, the cached values are cleared.
+    /// When the app enters the background, the `lastActiveTime` is recorded. When
+    /// the app returns to the foreground, the last active time is checked. If the
+    /// session has expired, the cached values are cleared.
     private func addAppPhaseObserver() {
         cancellable = appPhase.receive.sink { [weak self] appPhase in
             guard let self else { return }
 
             switch appPhase {
                 case .background:
-                    lastActiveUptime.saveValue()
+                    lastActiveTime.saveValue()
                 case .willEnterForeground:
-                    if lastActiveUptime.elapsed(sessionExpirationDuration) {
+                    if lastActiveTime.elapsed(inactivityDuration) {
                         // Reset the cache for a new session
                         storage.withValue { $0.removeAll() }
                     }
