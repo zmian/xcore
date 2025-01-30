f62d83bfa84f5f5c2a5975f09673ba86c262c3c4
Zeeshan Mian <zmian@me.com>
2025-01-30 18:37:08 +1100

    Button+Convenience updated to use the Convenience inits to preserve the accessibility

--- Sources/Xcore/SwiftUI/Components/Button/Button+Convenience.swift
+++ Sources/Xcore/SwiftUI/Components/Button/Button+Convenience.swift
@@ -15,218 +15,166 @@ extension Button<Text> {
     /// An accessible button that has the given `text` as label and performs the
     /// provided `action`.
     public static func accessible(
-        text: String,
+        _ text: String,
         action: @escaping () -> Void
     ) -> some View {
-        Button(action: action) {
-            Text(text)
-        }
-        .accessibilityIdentifier("\(text.camelcased())Button")
+        Button(text, action: action)
+            .accessibilityIdentifier("\(text.camelcased())Button")
     }
 
     /// A button with `OK` label and given action.
     public static func okay(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.ok)
-        }
-        .accessibilityIdentifier("okayButton")
+        Button(L.ok, action: action)
+            .accessibilityIdentifier("okayButton")
     }
 
     /// A button with `Yes` label and given action.
     public static func yes(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.yes)
-        }
-        .accessibilityIdentifier("yesButton")
+        Button(L.yes, action: action)
+            .accessibilityIdentifier("yesButton")
     }
 
     /// A button with `No` label and given action.
     public static func no(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.no)
-        }
-        .accessibilityIdentifier("noButton")
+        Button(L.no, action: action)
+            .accessibilityIdentifier("noButton")
     }
 
     /// A button with `Submit` label and given action.
     public static func submit(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.submit)
-        }
-        .accessibilityIdentifier("submitButton")
+        Button(L.submit, action: action)
+            .accessibilityIdentifier("submitButton")
     }
 
     /// A button with `Done` label and given action.
     public static func done(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.done)
-        }
-        .accessibilityIdentifier("doneButton")
+        Button(L.done, action: action)
+            .accessibilityIdentifier("doneButton")
     }
 
     /// A button with `Cancel` label and given action.
     public static func cancel(action: @escaping () -> Void) -> some View {
-        Button(role: .cancel, action: action) {
-            Text(L.cancel)
-        }
+        Button(L.cancel, role: .cancel, action: action)
         .accessibilityIdentifier("cancelButton")
     }
 
     /// A button with `Enable` label and given action.
     public static func enable(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.enable)
-        }
-        .accessibilityIdentifier("enableButton")
+        Button(L.enable, action: action)
+            .accessibilityIdentifier("enableButton")
     }
 
     /// A button with `Hide` label and given action.
     public static func hide(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.hide)
-        }
-        .accessibilityIdentifier("hideButton")
+        Button(L.hide, action: action)
+            .accessibilityIdentifier("hideButton")
     }
 
     /// A button with `Remove` label and given action.
     public static func remove(action: @escaping () -> Void) -> some View {
-        Button(role: .destructive, action: action) {
-            Text(L.remove)
-        }
-        .accessibilityIdentifier("removeButton")
+        Button(L.remove, role: .destructive, action: action)
+            .accessibilityIdentifier("removeButton")
     }
 
     /// A button with `Retry` label and given action.
     public static func retry(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.retry)
-        }
-        .accessibilityIdentifier("retryButton")
+        Button(L.retry, action: action)
+            .accessibilityIdentifier("retryButton")
     }
 
     /// A button with `Unlink` label and given action.
     public static func unlink(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.unlink)
-        }
-        .accessibilityIdentifier("unlinkButton")
+        Button(L.unlink, action: action)
+            .accessibilityIdentifier("unlinkButton")
     }
 
     /// A button with `Unlock` label and given action.
     public static func unlock(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.unlock)
-        }
-        .accessibilityIdentifier("unlockButton")
+        Button(L.unlock, action: action)
+            .accessibilityIdentifier("unlockButton")
     }
 
     /// A button with `Continue` label and given action.
     public static func `continue`(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.continue)
-        }
-        .accessibilityIdentifier("continueButton")
+        Button(L.continue, action: action)
+            .accessibilityIdentifier("continueButton")
     }
 
     /// A button with `Resend` label and given action.
     public static func resend(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.resend)
-        }
-        .accessibilityIdentifier("resendButton")
+        Button(L.resend, action: action)
+            .accessibilityIdentifier("resendButton")
     }
 
     /// A button with `Next` label and given action.
     public static func next(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.next)
-        }
-        .accessibilityIdentifier("nextButton")
+        Button(L.next, action: action)
+            .accessibilityIdentifier("nextButton")
     }
 
     /// A button with `Get Started` label and given action.
     public static func getStarted(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.getStarted)
-        }
-        .accessibilityIdentifier("getStartedButton")
+        Button(L.getStarted, action: action)
+            .accessibilityIdentifier("getStartedButton")
     }
 
     /// A button with `Help` label and given action.
     public static func help(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.help)
-        }
-        .accessibilityIdentifier("helpButton")
+        Button(L.help, action: action)
+            .accessibilityIdentifier("helpButton")
     }
 
     /// A button with `Contact Support` label and given action.
     public static func contactSupport(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.contactSupport)
-        }
-        .accessibilityIdentifier("contactSupportButton")
+        Button(L.contactSupport, action: action)
+            .accessibilityIdentifier("contactSupportButton")
     }
 
     /// A button with `Open App Settings` label and given action.
     public static func openAppSettings(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.openAppSettings)
-        }
-        .accessibilityIdentifier("openAppSettingsButton")
+        Button(L.openAppSettings, action: action)
+            .accessibilityIdentifier("openAppSettingsButton")
     }
 
     /// A button with `Track` label and given action.
     public static func track(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.track)
-        }
-        .accessibilityIdentifier("trackButton")
+        Button(L.track, action: action)
+            .accessibilityIdentifier("trackButton")
     }
 
     /// A button with `Start` label and given action.
     public static func start(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.start)
-        }
-        .accessibilityIdentifier("startButton")
+        Button(L.start, action: action)
+            .accessibilityIdentifier("startButton")
     }
 
     /// A button with `Share` label and given action.
     public static func share(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.share)
-        }
-        .accessibilityIdentifier("shareButton")
+        Button(L.share, action: action)
+            .accessibilityIdentifier("shareButton")
     }
 
     /// A button with `Sign Up` label and given action.
     public static func signup(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.signup)
-        }
-        .accessibilityIdentifier("signupButton")
+        Button(L.signup, action: action)
+            .accessibilityIdentifier("signupButton")
     }
 
     /// A button with `Log In` label and given action.
     public static func signin(action: @escaping () -> Void) -> some View {
-        Button(action: action) {
-            Text(L.signin)
-        }
-        .accessibilityIdentifier("signinButton")
+        Button(L.signin, action: action)
+            .accessibilityIdentifier("signinButton")
     }
 }
 
 // MARK: - Image
 
-extension Button<Image> {
+extension Button<Label<Text, Image>> {
     /// A button with `X` label and given action.
     public static func dismiss(action: @escaping () -> Void) -> some View {
-        Button(role: .cancel, action: action) {
-            Image(system: .xMark)
-        }
-        .accessibilityLabel("Dismiss")
-        .accessibilityIdentifier("dismissButton")
+        Button("Dismiss", systemImage: "xmark", role: .cancel, action: action)
+            .labelsHidden()
+            .accessibilityIdentifier("dismissButton")
     }
 }
 
