a8f99e29b6557c599f0c0c63c4d0c544adb48051
Zeeshan Mian <zmian@me.com>
2025-02-02 01:00:21 +1100

    updated docs

--- Sources/Xcore/Swift/Components/Observer.swift
+++ Sources/Xcore/Swift/Components/Observer.swift
@@ -6,17 +6,35 @@
 
 import Foundation
 
+/// A container for managing a list of observers, allowing objects to register
+/// for notifications and be automatically cleaned up when they are deallocated.
 open class Observers {
-    /// A list of all observers.
+    /// A list of registered observers.
     private var observers = [Observer]()
 
+    /// Initializes a new instance of `Observers`.
     public init() {
         // Must have a public init for client code to initialize this class.
     }
 
     // MARK: - Observer API
 
-    /// Register an observer.
+    /// Registers an observer with a notification handler.
+    ///
+    /// If the observer already exists, the handler is updated; otherwise, a new
+    /// observer is added.
+    ///
+    /// **Usage**
+    ///
+    /// ```swift
+    /// observers.observe(owner: self) {
+    ///     print("Notified!")
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - owner: The observing object.
+    ///   - handler: A closure invoked when the observer is notified.
     open func observe<T>(owner: T, _ handler: @escaping () -> Void) where T: AnyObject, T: Equatable {
         if let existingObserverIndex = observers.firstIndex(where: { $0 == owner }) {
             observers[existingObserverIndex].handler = handler
@@ -25,7 +43,9 @@ open class Observers {
         }
     }
 
-    /// Remove given observers.
+    /// Removes the given owner from the list of observers.
+    ///
+    /// - Parameter owner: The observing object to be removed.
     open func remove<T>(_ owner: T) where T: AnyObject, T: Equatable {
         guard let index = observers.firstIndex(where: { $0 == owner }) else {
             return
@@ -34,23 +54,25 @@ open class Observers {
         observers.remove(at: index)
     }
 
-    /// Remove all observers.
+    /// Removes all observers from the list.
     open func removeAll() {
         observers.removeAll(keepingCapacity: false)
     }
 
-    /// Removes all observers where the `owner` is deallocated.
+    /// Cleans up observers where the `owner` has been deallocated.
     open func compacted() {
         for (index, observer) in observers.enumerated().reversed() where observer.owner == nil {
             observers.remove(at: index)
         }
     }
 
+    /// The number of active observers.
     open var count: Int {
         compacted()
         return observers.count
     }
 
+    /// A Boolean property indicating whether there are no active observers.
     open var isEmpty: Bool {
         compacted()
         return observers.isEmpty
@@ -58,31 +80,42 @@ open class Observers {
 
     // MARK: - Notify API
 
-    /// A Boolean property indicating whether the notifications should be sent.
+    /// A Boolean property indicating whether notifications should be sent.
     ///
-    /// This flag is checked before firing any notification calls to the registered
-    /// observers. The default value is `true`.
+    /// This flag is checked before notifying registered observers.
     open var isNotificationEnabled = true
 
-    /// Invokes each registered observer if `oldValue` is different than `newValue`.
+    /// Notifies observers if `oldValue` is different from `newValue`.
+    ///
+    /// - Parameters:
+    ///   - oldValue: The previous value.
+    ///   - newValue: The new value to compare.
     open func notifyIfNeeded<T>(_ oldValue: T, newValue: T) where T: Equatable {
         guard oldValue != newValue else { return }
         notify()
     }
 
-    /// Invokes each registered observer if `oldValue` is different than `newValue`.
+    /// Notifies observers if `oldValue` is different from `newValue` (optional values).
+    ///
+    /// - Parameters:
+    ///   - oldValue: The previous value.
+    ///   - newValue: The new value to compare.
     open func notifyIfNeeded<T>(_ oldValue: T?, newValue: T?) where T: Equatable {
         guard oldValue != newValue else { return }
         notify()
     }
 
-    /// Invokes each registered observer if `oldValue` is different than `newValue`.
+    /// Notifies observers if `oldValue` is different from `newValue` (array values).
+    ///
+    /// - Parameters:
+    ///   - oldValue: The previous array value.
+    ///   - newValue: The new array value to compare.
     open func notifyIfNeeded<T>(_ oldValue: [T], newValue: [T]) where T: Equatable {
         guard oldValue != newValue else { return }
         notify()
     }
 
-    /// Invokes each registered observer.
+    /// Notifies all registered observers.
     open func notify() {
         compacted()
         guard isNotificationEnabled else { return }
@@ -92,11 +125,17 @@ open class Observers {
 
 // MARK: - Observer
 
+/// Represents an individual observer.
 private final class Observer {
     fileprivate let equals: (AnyObject) -> Bool
     weak var owner: AnyObject?
     var handler: (() -> Void)?
 
+    /// Initializes an `Observer` with an owner and a notification handler.
+    ///
+    /// - Parameters:
+    ///   - owner: The observing object.
+    ///   - handler: The closure invoked when the observer is notified.
     init<T>(owner: T, handler: @escaping () -> Void) where T: AnyObject, T: Equatable {
         self.owner = owner
         self.handler = handler
