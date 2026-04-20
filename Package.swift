4f6497993831914cc2878b445bd4dc853e7bbe42
Zeeshan Mian <zmian@me.com>
2026-04-20 12:45:10 +1000

    Add support for Xcode 26

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
         .library(name: "Xcore", targets: ["Xcore"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.17.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.21.7"),
         .package(url: "https://github.com/zmian/AnyCodable", branch: "master"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.6.3"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.12.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.0")
     ],
     targets: [
         .target(
