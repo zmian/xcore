c684c545516ac9949104f61d94c612b032b2cea5
Zeeshan Mian <zmian@me.com>
2026-04-20 21:15:49 +1000

    rm

--- Sources/Xcore/Cocoa/Extensions/AVFoundation+Extensions.swift
+++ Sources/Xcore/Cocoa/Extensions/AVFoundation+Extensions.swift
@@ -68,33 +68,4 @@ extension AVPlayerItem {
         self.init(url: url)
     }
 }
-
-// MARK: - AVAsset
-
-extension AVAsset {
-    /// Creates an ``AVAsset`` with local resource referenced file name.
-    ///
-    /// - Parameters:
-    ///   - filename: The local file name.
-    ///   - bundle: The bundle containing the specified file name. If you specify
-    ///     `nil`, this method looks in the main bundle of the current application.
-    /// - Returns: An instance of AVAsset.
-    public convenience init?(filename: String, bundle: Bundle? = nil) {
-        guard let url = (bundle ?? .main).url(filename: filename) else {
-            return nil
-        }
-
-        self.init(url: url)
-    }
-
-    /// Automatically detect and load the asset from local or a remote url.
-    public convenience init?(string: String) {
-        guard let url = URL(string: string), url.host != nil else {
-            self.init(filename: string)
-            return
-        }
-
-        self.init(url: url)
-    }
-}
 #endif
