ebf5dad3f5b0d9d038dee32dd29acca522c68755
Zeeshan Mian <zmian@me.com>
2026-09-16 22:34:50 -0400

    m

--- Tests/XcoreTests/URLTests.swift
+++ Tests/XcoreTests/URLTests.swift
@@ -10,22 +10,22 @@ import Foundation
 
 struct URLTests {
     @Test
-    func queryItem() {
-        let url = URL(string: "https://example.com/?q=HelloWorld")!
+    func queryItem() throws {
+        let url = try #require(URL(string: "https://example.com/?q=HelloWorld"))
         #expect(url.queryItem(named: "q") == "HelloWorld")
         #expect(url.queryItem(named: "x") == nil)
     }
 
     @Test
-    func removingFragment() {
-        let url1 = URL(string: "https://example.com/#hero")!
+    func removingFragment() throws {
+        let url1 = try #require(URL(string: "https://example.com/#hero"))
         #expect(url1.removingFragment() == URL(string: "https://example.com/")!)
 
-        let url2 = URL(string: "https://example.com#hero")!
+        let url2 = try #require(URL(string: "https://example.com#hero"))
         #expect(url2.removingFragment() == URL(string: "https://example.com")!)
 
         // Unchanged url.
-        let url3 = URL(string: "https://example.com/?q=HelloWorld")!
+        let url3 = try #require(URL(string: "https://example.com/?q=HelloWorld"))
         #expect(url3.removingFragment() == url3)
     }
 
@@ -42,14 +42,14 @@ struct URLTests {
     }
 
     @Test
-    func removingScheme() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func removingScheme() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.removingScheme() == "www.example.com/?q=HelloWorld")
         #expect(url2.removingScheme() == "welcome.hello.example.com/?q=HelloWorld")
@@ -61,14 +61,14 @@ struct URLTests {
     }
 
     @Test
-    func removingQueryItems() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func removingQueryItems() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.removingQueryItems() == URL(string: "https://www.example.com/"))
         #expect(url2.removingQueryItems() == URL(string: "http://welcome.hello.example.com/"))
@@ -80,15 +80,15 @@ struct URLTests {
     }
 
     @Test
-    func removingQueryItemsList() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
-        let url8 = URL(string: "https://www.example.com/?q=HelloWorld")!
+    func removingQueryItemsList() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
+        let url8 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
 
         #expect(url1.removingQueryItems(["q"]) == URL(string: "https://www.example.com/"))
         #expect(url2.removingQueryItems(["q", "lang"]) == URL(string: "http://welcome.hello.example.com/"))
@@ -101,14 +101,14 @@ struct URLTests {
     }
 
     @Test
-    func removingQueryItemNamed() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func removingQueryItemNamed() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.removingQueryItem(named: "q") == URL(string: "https://www.example.com/"))
         #expect(url2.removingQueryItem(named: "lang") == URL(string: "http://welcome.hello.example.com/?q=HelloWorld"))
@@ -120,14 +120,14 @@ struct URLTests {
     }
 
     @Test
-    func replacingQueryItemNamed() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func replacingQueryItemNamed() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.replacingQueryItem(named: "q", with: "Greetings") == URL(string: "https://www.example.com/?q=Greetings"))
         #expect(url2.replacingQueryItem(named: "lang", with: "Greetings") == URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=Greetings"))
@@ -141,14 +141,14 @@ struct URLTests {
     }
 
     @Test
-    func replacingQueryItemsList() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func replacingQueryItemsList() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.replacingQueryItems(["q"], with: "xxxx") == URL(string: "https://www.example.com/?q=xxxx"))
         #expect(url1.replacingQueryItems([], with: "xxxx") == URL(string: "https://www.example.com/?q=HelloWorld"))
@@ -163,14 +163,14 @@ struct URLTests {
     }
 
     @Test
-    func maskingAllQueryItems() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func maskingAllQueryItems() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.maskingAllQueryItems() == URL(string: "https://www.example.com/?q=xxxx"))
         #expect(url1.maskingAllQueryItems() == URL(string: "https://www.example.com/?q=xxxx"))
@@ -187,14 +187,14 @@ struct URLTests {
     }
 
     @Test
-    func appendingQueryItemsList() {
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift")!
-        let url3 = URL(string: "www.hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "hello.example.com")!
-        let url5 = URL(string: "mail.app")!
-        let url6 = URL(string: "file://mail.app")!
-        let url7 = URL(string: "mailto://mail.app")!
+    func appendingQueryItemsList() throws {
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "http://welcome.hello.example.com/?q=HelloWorld&lang=swift"))
+        let url3 = try #require(URL(string: "www.hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "hello.example.com"))
+        let url5 = try #require(URL(string: "mail.app"))
+        let url6 = try #require(URL(string: "file://mail.app"))
+        let url7 = try #require(URL(string: "mailto://mail.app"))
 
         #expect(url1.appendQueryItems([.init(name: "lang", value: "Swift")]) == URL(string: "https://www.example.com/?q=HelloWorld&lang=Swift"))
         #expect(url1.appendQueryItems([.init(name: "lang", value: "Swift")]) == URL(string: "https://www.example.com/?q=HelloWorld&lang=Swift"))
@@ -209,12 +209,12 @@ struct URLTests {
     }
 
     @Test
-    func matchFound() {
+    func matchFound() throws {
         let domain = "example.com"
-        let url1 = URL(string: "https://www.example.com/?q=HelloWorld")!
-        let url2 = URL(string: "https://example.com/?q=HelloWorld")!
-        let url3 = URL(string: "https://hello.example.com/?q=HelloWorld")!
-        let url4 = URL(string: "https://welcome.hello.example.com/?q=HelloWorld")!
+        let url1 = try #require(URL(string: "https://www.example.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "https://example.com/?q=HelloWorld"))
+        let url3 = try #require(URL(string: "https://hello.example.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "https://welcome.hello.example.com/?q=HelloWorld"))
 
         #expect(url1.host == "www.example.com")
         #expect(url2.host == "example.com")
@@ -228,8 +228,8 @@ struct URLTests {
         #expect(!url4.matches(domain, includingSubdomains: false))
 
         // Org
-        let url6 = URL(string: "https://example.org")!
-        let url7 = URL(string: "example.org")!
+        let url6 = try #require(URL(string: "https://example.org"))
+        let url7 = try #require(URL(string: "example.org"))
         #expect(url6.host == "example.org")
         #expect(url7.host == nil)
         #expect(url6.matches("example.org") == true)
@@ -237,13 +237,13 @@ struct URLTests {
     }
 
     @Test
-    func matchNotFound() {
+    func matchNotFound() throws {
         let domain = "example.com"
-        let url1 = URL(string: "https://www.examplex.com/?q=HelloWorld")!
-        let url2 = URL(string: "https://example_x.com/?q=HelloWorld")!
-        let url3 = URL(string: "https://hello.example-x.com/?q=HelloWorld")!
-        let url4 = URL(string: "https://welcome.hello.helloexample.com/?q=HelloWorld")!
-        let url5 = URL(string: "https://example.org")!
+        let url1 = try #require(URL(string: "https://www.examplex.com/?q=HelloWorld"))
+        let url2 = try #require(URL(string: "https://example_x.com/?q=HelloWorld"))
+        let url3 = try #require(URL(string: "https://hello.example-x.com/?q=HelloWorld"))
+        let url4 = try #require(URL(string: "https://welcome.hello.helloexample.com/?q=HelloWorld"))
+        let url5 = try #require(URL(string: "https://example.org"))
 
         #expect(url1.host == "www.examplex.com")
         #expect(url2.host == "example_x.com")
@@ -259,20 +259,20 @@ struct URLTests {
     }
 
     @Test
-    func maskingSensitiveQueryItems() {
+    func maskingSensitiveQueryItems() throws {
         FeatureFlag.resetProviders()
         FeatureFlag.setValue(["token", "vfp"], forKey: "sensitive_url_query_parameters")
-        let url1 = URL(string: "https://example.com/magiclink?token=Jn3yk23cf23")!
-        let url2 = URL(string: "https://example.com/magiclink?token=Jn3yk23cf23")!
-        let url3 = URL(string: "https://app.example.com/magiclink?token=Jn3yk23cf23")!
-        let url4 = URL(string: "https://example.com/callback/prove?vfp=Jn3yk23cf23")!
-        let url5 = URL(string: "https://app.example.com/magiclink?vfp=Jn3yk23cf23&token=Jn3yk23cf23")!
-        let url6 = URL(string: "https://app.example.com/magiclink?token=Jn3yk23cf23&vfp=Jn3yk23cf23")!
-        let url7 = URL(string: "https://example.com/dl/trade/buy?id=20")!
-        let url8 = URL(string: "https://example.com/dl/trade/buy?id=20&token=Jn3yk2x3cf23")!
-        let url9 = URL(string: "https://example.com/buy?token=Jn3yk2x3cf23")!
-        let url10 = URL(string: "https://example.com?token=Jn3yk2x3cf23")!
-        let url11 = URL(string: "https://example.com?code=Jn3yk2x3cf23")!
+        let url1 = try #require(URL(string: "https://example.com/magiclink?token=Jn3yk23cf23"))
+        let url2 = try #require(URL(string: "https://example.com/magiclink?token=Jn3yk23cf23"))
+        let url3 = try #require(URL(string: "https://app.example.com/magiclink?token=Jn3yk23cf23"))
+        let url4 = try #require(URL(string: "https://example.com/callback/prove?vfp=Jn3yk23cf23"))
+        let url5 = try #require(URL(string: "https://app.example.com/magiclink?vfp=Jn3yk23cf23&token=Jn3yk23cf23"))
+        let url6 = try #require(URL(string: "https://app.example.com/magiclink?token=Jn3yk23cf23&vfp=Jn3yk23cf23"))
+        let url7 = try #require(URL(string: "https://example.com/dl/trade/buy?id=20"))
+        let url8 = try #require(URL(string: "https://example.com/dl/trade/buy?id=20&token=Jn3yk2x3cf23"))
+        let url9 = try #require(URL(string: "https://example.com/buy?token=Jn3yk2x3cf23"))
+        let url10 = try #require(URL(string: "https://example.com?token=Jn3yk2x3cf23"))
+        let url11 = try #require(URL(string: "https://example.com?code=Jn3yk2x3cf23"))
 
         #expect(url1.maskingSensitiveQueryItems() == URL(string: "https://example.com/magiclink?token=xxxx"))
         #expect(url2.maskingSensitiveQueryItems() == URL(string: "https://example.com/magiclink?token=xxxx"))
@@ -288,8 +288,8 @@ struct URLTests {
     }
 
     @Test(.disabled(if: TestEnvironment.isCI, "Requires external redirect/WebKit behavior."))
-    func resolvingRedirectedLink() async {
-        let shortUrl = URL(string: "https://git.new/swift")!
+    func resolvingRedirectedLink() async throws {
+        let shortUrl = try #require(URL(string: "https://git.new/swift"))
         let resolvedUrl = await shortUrl.resolvingRedirectedLink(timeout: .seconds(15))
         let expandedUrl = "https://github.com/swiftlang/swift"
         #expect(resolvedUrl?.absoluteString == expandedUrl)
